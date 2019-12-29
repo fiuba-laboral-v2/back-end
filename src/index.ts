f9877110b0368e0b951b3e7d74581c10af5d2e32
Sebastian Blanco <sblanco@devartis.com>
2019-12-28 23:28:10 -0300

    Require routes and pass them the app

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
-const expressValidator = require('express-validator');
 
-const app: express.Application = express();
+
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/', require('./routes/root'));
+require('./routes/root')(app);
 
 module.exports = app;
