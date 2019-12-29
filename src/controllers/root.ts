1e14af0e5cbfd99ac27998f38d6b5c584af289dd
Sebastian Blanco <sblanco@devartis.com>
2019-12-28 23:27:05 -0300

    Make method public

--- src/controllers/root.ts
+++ src/controllers/root.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 const httpStatus = require('http-status-codes');
 
 class RootController {
-    index (req: Request, res: Response) {
+    public index (req: Request, res: Response) {
         return res.status(httpStatus.OK).json({})
     }
 }
