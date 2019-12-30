33715cb4c733111766a073673304050f872271d6
Sebastian Blanco <sblanco@devartis.com>
2019-12-30 17:36:07 -0300

    Fix import usage

--- spec/controllers/root.spec.ts
+++ spec/controllers/root.spec.ts
@@ -1,16 +1,19 @@
-import * as chai from "chai";
+import chai from "chai";
 import chaiHttp from "chai-http";
 import app from "../../src/index";
+import { OK } from "http-status-codes";
 
 const server = app.listen();
 
 chai.use(chaiHttp);
 
-it("gets a 200 status from '/'", (done) => {
-  chai.request(server)
-    .get("/")
-    .end((err, res) => {
-      res.should.have.status(200);
-      done();
-    });
+describe("Root", () => {
+  it("gets a 200 status from '/'", (done) => {
+    chai.request(server)
+      .get("/")
+      .end((err, res) => {
+        chai.expect(res.status).to.equal(OK);
+        done();
+      });
+  });
 });
