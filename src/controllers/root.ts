69b5a32fc45e73ba8346255b506fb61161bb10f6
Sebastian Blanco <sblanco@devartis.com>
2020-01-01 18:49:17 -0300

    Add post and get by id method using model

--- src/controllers/root.ts
+++ src/controllers/root.ts
@@ -1,8 +1,53 @@
 import { Request, Response } from "express";
-import { OK } from "http-status-codes";
+import { OK, CREATED, INTERNAL_SERVER_ERROR } from "http-status-codes";
+import model from "../models";
 
 const rootController = {
-    index: (req: Request, res: Response) => res.status(OK).json({})
+  findById: (req: Request, res: Response) => {
+        model.db.Root
+            .find({ where: { id: req.params.id } })
+            .then((root: object) => {
+              res
+                .status(OK)
+                .json({ success: true, data: root });
+          })
+            .catch((error: string) => {
+              res
+                .status(INTERNAL_SERVER_ERROR)
+                .json({ success: false, errors: error });
+          });
+    },
+    index: (req: Request, res: Response) => {
+      model.db.Root
+        .findAll({})
+        .then((root: object) => {
+          res
+            .status(OK)
+            .json({ success: true, data: root });
+        })
+        .catch((error: string) => {
+          res
+            .status(INTERNAL_SERVER_ERROR)
+            .json({ success: false, errors: error });
+        });
+    },
+    create: (req: Request, res: Response) => {
+        const { title } = req.body;
+        model.db.Root
+          .create({
+              title: title
+          })
+          .then((root: object) => {
+              res
+                .status(CREATED)
+                .json({ success: true, data: root });
+          })
+          .catch((error: string) => {
+              res
+                .status(INTERNAL_SERVER_ERROR)
+                .json({ success: false, errors: error });
+          });
+    }
 };
 
 export default rootController;
--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,10 +1,13 @@
 import { Express, Router } from "express";
+import { json } from "body-parser";
 import rootController from "../controllers/root";
 
 const RootRoute =  {
   set: (app: Express) => {
     const router = Router();
     router.get("/", rootController.index);
+    router.get("/:id?", rootController.findById);
+    router.post("/", json(), rootController.create);
     app.use("/", router);
   }
 };
