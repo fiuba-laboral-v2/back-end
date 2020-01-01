bb024606563afff79f0e6caed63cec21f020a7b9
Sebastian Blanco <sblanco@devartis.com>
2020-01-01 18:46:27 -0300

    Test root endpoint

--- test/controllers/root.test.ts
+++ test/controllers/root.test.ts
@@ -1,10 +1,44 @@
 import request from "supertest";
 import app from "../../src/app";
-import {OK} from "http-status-codes";
+import {OK, CREATED} from "http-status-codes";
+import model from "../../src/models";
 
 describe("Root path", () => {
-  test("It should give an OK status", async () => {
-    const response = await request(app).get("/");
-    expect(response.status).toEqual(OK);
+  const dummyTest = { title: "test" } as any;
+
+  beforeEach(() => {
+    return model.db.Root.destroy({ truncate: true })
+      .then(() => {
+        model.db.Root.create(dummyTest)
+          .then((record: any) => {
+            dummyTest.id = record.id;
+        });
+      });
+  });
+
+  afterAll(() => {
+    return model.close();
+  });
+
+  describe("GET", () => {
+    test("It should give an OK status", async () => {
+      const response = await request(app).get("/");
+      expect(response.status).toEqual(OK);
+      expect(response.body.data.length).toEqual(1);
+    });
+
+    test("It should give an OK status and return by id", async () => {
+      const response = await request(app).get(`/${dummyTest.id}`);
+      expect(response.status).toEqual(OK);
+      expect(response.body.data.title).toEqual(dummyTest.title);
+    });
+  });
+
+  describe("POST", () => {
+    test("It should give an CREATED status", async () => {
+      const response = await request(app).post("/").send({ title: "test" });
+      expect(response.status).toEqual(CREATED);
+      expect(response.body.data.title).toEqual(dummyTest.title);
+    });
   });
 });
