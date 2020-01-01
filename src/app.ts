5f562926f93508efca08a939edd997f760cf7451
Sebastian Blanco <sblanco@devartis.com>
2020-01-01 16:48:10 -0300

    Move database base model to a class

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { json, urlencoded } from "body-parser";
 import RootRoute from "./routes/root";
+import model from "./models";
 
 const app: express.Express = express();
 
 app.use(json());
 app.use(urlencoded({extended: true}));
+
+model.set();
 
 RootRoute.set(app);
 
--- src/models/index.ts
+++ src/models/index.ts
@@ -7,35 +7,38 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || process.env.DEVELOPMENT;
 import databaseJSON from "../../config/database.json";
 
-const db = {} as any;
 
-const config = databaseJSON[env];
+class Model {
+  public model = {} as any;
+  public set() {
+    const config = databaseJSON[env];
 
-let sequelize: Sequelize;
+    let sequelize: Sequelize;
 
-if (config.USE_ENV_VARIABLE) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+    if (config.USE_ENV_VARIABLE) {
+      sequelize = new Sequelize(process.env.DATABASE_URL, config);
+    } else {
+      sequelize = new Sequelize(config.database, config.username, config.password, config);
+    }
+
+    fs
+      .readdirSync(__dirname)
+      .filter(file => {
+        return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
+      })
+      .forEach(file => {
+        const model = sequelize.import(path.join(__dirname, file));
+        this.model[model.name] = model;
+      });
+
+    Object.keys(this.model).forEach(modelName => {
+      if (this.model[modelName].associate) {
+        this.model[modelName].associate(this.model);
+      }
+    });
+    this.model.sequelize = sequelize;
+    this.model.Sequelize = Sequelize;
+  }
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
-  })
-  .forEach(file => {
-    const model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+export default new Model();
