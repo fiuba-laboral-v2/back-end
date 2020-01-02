919b41af6d664d39f2aafa40b0708c61d91565fa
Sebastian Blanco <sblanco@devartis.com>
2020-01-02 01:31:55 -0300

    Use typescript sequelize cli

--- src/migrations/20200101200813-create-root.ts
+++ src/migrations/20200101200813-create-root.ts
@@ -1,27 +1,32 @@
-'use strict';
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Roots', {
+import { QueryInterface, SequelizeStatic } from "sequelize";
+
+export = {
+  up: (queryInterface: QueryInterface, Sequelize: SequelizeStatic) => {
+    return queryInterface.createTable("Roots", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+
       title: {
         type: Sequelize.STRING
       },
+
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
       },
+
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
       }
     });
   },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Roots');
+
+  down: (queryInterface: QueryInterface, Sequelize: SequelizeStatic) => {
+    return queryInterface.dropTable("Roots");
   }
 };
---
+++ src/models/root.ts
@@ -0,0 +1,22 @@
+import { Sequelize, DataTypes } from "sequelize";
+
+export interface RootsAttributes {
+  title?: string;
+}
+
+export interface RootsInstance {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+  title: string;
+}
+
+module.exports = (sequelize: Sequelize, dataTypes: DataTypes) => {
+  const Roots = sequelize.define("Roots", {
+    title: dataTypes.STRING
+  });
+  Roots.associate = models => {
+    // associations can be defined here
+  };
+  return Roots;
+};
