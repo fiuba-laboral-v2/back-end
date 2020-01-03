452be5ce4df21df61d787092caff53af2b3855a0
Sebastian Blanco <sblanco@devartis.com>
2020-01-02 23:51:52 -0300

    Log current environment

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,9 @@ import express from "express";
 import { json, urlencoded } from "body-parser";
 import RootRoute from "./routes/root";
 import model from "./models";
+import {Logger} from "./libs/logger";
 
+Logger.info(`Running on ${process.env.NODE_ENV} environment`);
 const app: express.Express = express();
 
 app.use(json());
