deb4e58aa7fb620a116adfc655bd8fb07aeb0f48
Sebastian Blanco <sblanco@devartis.com>
2020-01-04 16:46:06 -0300

    Fix identation and error message

--- src/roots/roots_controller.ts
+++ src/roots/roots_controller.ts
@@ -7,21 +7,21 @@ const rootController = {
     try {
       return res.status(OK).json({ data: await RootsRepository.findById(req.params.id) });
     } catch (e) {
-      return res.status(INTERNAL_SERVER_ERROR).json({ data: e });
+      return res.status(INTERNAL_SERVER_ERROR).json({ error: "An internal error has occurred" });
     }
   },
   index: async (req: Request, res: Response) => {
     try {
       return res.status(OK).json({ data: await RootsRepository.findAll() });
     } catch (e) {
-     return res.status(INTERNAL_SERVER_ERROR).json({ data: e });
+      return res.status(INTERNAL_SERVER_ERROR).json({ error: "An internal error has occurred" });
     }
   },
   create: async (req: Request, res: Response) => {
     try {
       return res.status(CREATED).json({ data: await RootsRepository.create(req.body) });
     } catch (e) {
-     return res.status(INTERNAL_SERVER_ERROR).json({ data: e });
+      return res.status(INTERNAL_SERVER_ERROR).json({ error: "An internal error has occurred" });
     }
   }
 };
