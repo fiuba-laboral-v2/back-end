a23bf81cc8a8733a9d0695ab7265ffde702e6856
Sebastian Blanco <sblanco@devartis.com>
2020-01-04 16:47:05 -0300

    Rename exception var

--- src/roots/roots_controller.ts
+++ src/roots/roots_controller.ts
@@ -6,21 +6,21 @@ const rootController = {
   findById: async (req: Request, res: Response) => {
     try {
       return res.status(OK).json({ data: await RootsRepository.findById(req.params.id) });
-    } catch (e) {
+    } catch (exception) {
       return res.status(INTERNAL_SERVER_ERROR).json({ error: "An internal error has occurred" });
     }
   },
   index: async (req: Request, res: Response) => {
     try {
       return res.status(OK).json({ data: await RootsRepository.findAll() });
-    } catch (e) {
+    } catch (exception) {
       return res.status(INTERNAL_SERVER_ERROR).json({ error: "An internal error has occurred" });
     }
   },
   create: async (req: Request, res: Response) => {
     try {
       return res.status(CREATED).json({ data: await RootsRepository.create(req.body) });
-    } catch (e) {
+    } catch (exception) {
       return res.status(INTERNAL_SERVER_ERROR).json({ error: "An internal error has occurred" });
     }
   }
