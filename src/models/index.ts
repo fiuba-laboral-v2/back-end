06c5ec85f747d48d6716c5458eb86ccfb02a6f91
Sebastian Blanco <sblanco@devartis.com>
2020-01-03 23:15:31 -0300

    Fix database config

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,51 +1,25 @@
 "use strict";
 
-import fs from "fs";
-import path from "path";
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 const env = process.env.NODE_ENV || "development";
 import databaseJSON from "../../config/database.json";
+import Roots from "../roots/roots";
 
 
-class Model {
-  public db = {} as any;
+export default class Model {
+  public static sequelize: Sequelize;
 
-  public close() {
-    this.db.sequelize.close();
+  public static close() {
+    this.sequelize.close();
   }
 
-  public set() {
+  public static set() {
     const config = databaseJSON[env];
-
-    let sequelize: Sequelize;
-
     if (config.use_env_variable) {
-      sequelize = new Sequelize(process.env.DATABASE_URL, config);
+      this.sequelize = new Sequelize(process.env.DATABASE_URL, config);
     } else {
-      sequelize = new Sequelize(config.database, config.username, config.password, config);
+      this.sequelize = new Sequelize(config.database, config.username, config.password, config);
     }
-
-    const basename = path.basename(__filename);
-
-    fs
-      .readdirSync(__dirname)
-      .filter(file => {
-        const extensionMatches = [".js", ".ts"].includes(file.slice(-3));
-        return (file.indexOf(".") !== 0) && (file !== basename) && (extensionMatches);
-      })
-      .forEach(file => {
-        const model = sequelize.import(path.join(__dirname, file));
-        this.db[model.name] = model;
-      });
-
-    Object.keys(this.db).forEach(modelName => {
-      if (this.db[modelName].associate) {
-        this.db[modelName].associate(this.db);
-      }
-    });
-    this.db.sequelize = sequelize;
-    this.db.Sequelize = Sequelize;
+    this.sequelize.addModels([Roots]);
   }
 }
-
-export default new Model();
