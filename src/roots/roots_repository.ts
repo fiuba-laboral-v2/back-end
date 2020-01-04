b82d47a5fb5579d635142e5e31a471db261549d6
Sebastian Blanco <sblanco@devartis.com>
2020-01-04 16:49:46 -0300

    Rename exception var

--- src/roots/roots_repository.ts
+++ src/roots/roots_repository.ts
@@ -4,29 +4,29 @@ export const RootsRepository = {
   findById: async (id: string) => {
     try {
       return await Roots.findOne({where: {id: id}});
-    } catch (e) {
-      throw e;
+    } catch (exception) {
+      throw exception;
     }
   },
   findAll: async () => {
     try {
       return await Roots.findAll({});
-    } catch (e) {
-      throw e;
+    } catch (exception) {
+      throw exception;
     }
   },
   create: async (record: object) => {
     try {
       return await Roots.create(record);
-    } catch (e) {
-      throw e;
+    } catch (exception) {
+      throw exception;
     }
   },
   truncate: async () => {
     try {
       return await Roots.destroy({truncate: true});
-    } catch (e) {
-      throw e;
+    } catch (exception) {
+      throw exception;
     }
   }
 };
