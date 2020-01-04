a2b089e24d0657c637c83e2961ef8b321baa59be
Sebastian Blanco <sblanco@devartis.com>
2020-01-03 23:16:19 -0300

    Import class

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { json, urlencoded } from "body-parser";
 import RootRoute from "./routes/root";
-import model from "./models";
+import Model from "./models";
 import {Logger} from "./libs/logger";
 
 Logger.info(`Running on ${process.env.NODE_ENV} environment`);
@@ -10,7 +10,7 @@ const app: express.Express = express();
 app.use(json());
 app.use(urlencoded({extended: true}));
 
-model.set();
+Model.set();
 
 RootRoute.set(app);
 
