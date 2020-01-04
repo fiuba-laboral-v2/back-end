aae48378ad0357b09eecd3f046fb5f1e139d6ad0
Sebastian Blanco <sblanco@devartis.com>
2020-01-04 02:25:54 -0300

    Throw catched exception

--- src/roots/roots_repository.ts
+++ src/roots/roots_repository.ts
@@ -5,28 +5,28 @@ export const RootsRepository = {
       try {
         return await Roots.findOne({ where: { id: id } });
       } catch (e) {
-        throw new Error(e.message);
+        throw e;
       }
   },
   findAll: async () => {
     try {
       return await Roots.findAll({});
     } catch (e) {
-      throw new Error(e.message);
+      throw e;
     }
   },
   create: async (record: object) => {
     try {
       return await Roots.create(record);
     } catch (e) {
-      throw new Error(e.message);
+      throw e;
     }
   },
   truncate: async () => {
     try {
       return await Roots.destroy({ truncate: true });
     } catch (e) {
-      throw new Error(e.message);
+      throw e;
     }
   }
 };
