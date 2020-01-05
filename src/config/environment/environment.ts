97dcff3f60a18076a40130ce6ddd8b2cf544e1bc
Sebastian Blanco <sblanco@devartis.com>
2020-01-05 19:24:30 -0300

    Use object instead of class

--- src/config/environment/environment.ts
+++ src/config/environment/environment.ts
@@ -1,9 +1,9 @@
-export default class Environment {
-  public static PRODUCTION: string = "production";
-  public static STAGING: string = "staging";
-  public static DEVELOPMENT: string = "development";
-  public static TEST_TRAVIS: string = "test_travis";
-  public static TEST: string = "test";
-  public static NODE_ENV: string = process.env.NODE_ENV || Environment.DEVELOPMENT;
-  public static DATABASE_URL: string =  process.env.DATABASE_URL || "";
-}
+export default {
+  PRODUCTION: "production",
+  STAGING: "staging",
+  DEVELOPMENT: "development",
+  TEST_TRAVIS: "test_travis",
+  TEST: "test",
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DATABASE_URL: process.env.DATABASE_URL || ""
+};
