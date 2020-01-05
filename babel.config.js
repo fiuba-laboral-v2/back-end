12edb6e1b999c190ba50513cbe8d95ca14124993
Sebastian Blanco <sblanco@devartis.com>
2020-01-04 21:45:14 -0300

    Change properties config

--- babel.config.js
+++ babel.config.js
@@ -12,6 +12,6 @@ module.exports = {
   ],
   "plugins": [
     ["@babel/plugin-proposal-decorators", {"legacy": true}],
-    ["@babel/plugin-proposal-class-properties", {"loose": false}],
+    ["@babel/plugin-proposal-class-properties", {"loose": true}],
   ]
 };
