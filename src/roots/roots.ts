7e6d009dec3c8522a413755e133a083dfe973e26
Sebastian Blanco <sblanco@devartis.com>
2020-01-05 18:28:42 -0300

    Remove constructor

--- src/roots/roots.ts
+++ src/roots/roots.ts
@@ -4,10 +4,5 @@ import {Table, Column, Model, DataType, AllowNull} from "sequelize-typescript";
 export default class Roots extends Model<Roots> {
   @AllowNull(false)
   @Column(DataType.TEXT)
-  public title!: string;
-
-  constructor(record?: any) {
-    super(record);
-    this.title = record!.title!;
-  }
+  public title: string;
 }
