56ad37d5d702fc9daade67d343df9e5e6041fdfb
dylan <dylanalvarez1995@gmail.com>
2020-01-22 00:25:42 -0300

    Make ApolloTestClient follow new convention

--- test/graphql/Translation/queries/translation.test.ts
+++ test/graphql/Translation/queries/translation.test.ts
@@ -1,5 +1,5 @@
 import { gql } from "apollo-server";
-import { executeQuery } from "../../apolloTestClient";
+import { executeQuery } from "../../ApolloTestClient";
 
 const query = gql`
   query ($path: String!) {
