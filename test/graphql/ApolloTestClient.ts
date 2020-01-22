cc04e250f345dbd98f09439ae0c15662b3c7f47d
dylan <dylanalvarez1995@gmail.com>
2020-01-22 00:29:27 -0300

    Make ApolloTestClient's variables parameter optional

--- test/graphql/ApolloTestClient.ts
+++ test/graphql/ApolloTestClient.ts
@@ -4,14 +4,14 @@ import { DocumentNode } from "graphql";
 
 const testClient = createTestClient(apolloServer as any);
 
-const executeQuery = (query: DocumentNode, variables: object) => {
+const executeQuery = (query: DocumentNode, variables?: object) => {
   return testClient.query({
     query: query,
     variables: variables
   });
 };
 
-const executeMutation = (mutation: DocumentNode, variables: object) => {
+const executeMutation = (mutation: DocumentNode, variables?: object) => {
   return testClient.mutate({
     mutation: mutation,
     variables: variables
