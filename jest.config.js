05a4d8b7a06e3aa42956dbf5cbc903e666d1bb04
Sebastian Blanco <sblanco@devartis.com>
2020-01-27 19:16:20 -0300

    Rollback ts config and test cache

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      isolatedModules: true
-    }
-  },
   coveragePathIgnorePatterns: [
       "node_modules",
       "test"
