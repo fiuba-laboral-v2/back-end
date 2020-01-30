81873e7f7340a60301bca16842d2f4040ca7281e
Sebastian Blanco <sblanco@devartis.com>
2020-01-29 21:05:18 -0300

    Test CompanyProfilePhoneNumber valid creation

---
+++ test/models/CompanyProfilePhoneNumber/CompanyProfilePhoneNumber.test.ts
@@ -0,0 +1,23 @@
+import { CompanyProfilePhoneNumber } from "../../../src/models/CompanyProfilePhoneNumber";
+import Database from "../../../src/config/Database";
+
+beforeAll(async () => {
+  await Database.setConnection();
+});
+
+beforeEach(async () => {
+  await CompanyProfilePhoneNumber.destroy({ truncate: true });
+});
+
+afterAll(async () => {
+  await Database.close();
+});
+
+test("create a valid CompanyProfilePhoneNumber", async () => {
+  const companyProfilePhoneNumber: CompanyProfilePhoneNumber = new CompanyProfilePhoneNumber({
+    phoneNumber: 43076555,
+    companyProfileId: 0
+  });
+  expect(companyProfilePhoneNumber).not.toBeNull();
+  expect(companyProfilePhoneNumber).not.toBeUndefined();
+});
