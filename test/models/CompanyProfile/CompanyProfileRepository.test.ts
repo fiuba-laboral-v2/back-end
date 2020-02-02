8ecfd63e37cbb64b4f00911947ea2d09d9066978
Sebastian Blanco <sblanco@devartis.com>
2020-02-02 01:14:26 -0300

    Use methods to avoid repeating code

--- test/models/CompanyProfile/CompanyProfileRepository.test.ts
+++ test/models/CompanyProfile/CompanyProfileRepository.test.ts
@@ -10,6 +10,45 @@ import {
 import Database from "../../../src/config/Database";
 
 describe("CompanyProfileRepository", () => {
+  const companyProfileData = () => {
+    return {
+      cuit: "30711819017",
+      companyName: "devartis",
+      slogan: "We craft web applications for great businesses",
+      description: "some description",
+      logo: "https://pbs.twimg.com/profile_images/1039514458282844161/apKQh1fu_400x400.jpg"
+    };
+  };
+
+  const buildPhoneNumbers = () => {
+    return CompanyProfilePhoneNumberRepository.build(
+      [
+        43076555,
+        43076556,
+        43076455,
+        43076599
+      ]
+    );
+  };
+
+  const buildPhotos = () => {
+    return CompanyProfilePhotoRepository.build(
+      [
+        `data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//
+        8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==`,
+        `data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//
+        8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==`
+      ]
+    );
+  };
+
+  const createCompanyProfile = async () => {
+    const companyProfile: CompanyProfile = new CompanyProfile(companyProfileData());
+    const phoneNumbers: CompanyProfilePhoneNumber[] = buildPhoneNumbers();
+    const photos: CompanyProfilePhoto[] = buildPhotos();
+    await CompanyProfileRepository.save(companyProfile, phoneNumbers, photos);
+    return { companyProfile, phoneNumbers, photos };
+  };
 
   beforeAll(async () => {
     await Database.setConnection();
@@ -24,37 +63,12 @@ describe("CompanyProfileRepository", () => {
   });
 
   test("create a new companyProfile", async () => {
-    const companyProfileData = {
-      cuit: "30711819017",
-      companyName: "devartis",
-      slogan: "We craft web applications for great businesses",
-      description: "some description",
-      logo: "https://pbs.twimg.com/profile_images/1039514458282844161/apKQh1fu_400x400.jpg"
-    };
-    const companyProfile: CompanyProfile = new CompanyProfile(companyProfileData);
-    const phoneNumbers: CompanyProfilePhoneNumber[] = CompanyProfilePhoneNumberRepository.build(
-      [
-        43076555,
-        43076556,
-        43076455,
-        43076599
-      ]
-    );
-    const photos: CompanyProfilePhoto[] = CompanyProfilePhotoRepository.build(
-      [
-        `data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//
-        8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==`,
-        `data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//
-        8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==`
-      ]
-    );
-    await CompanyProfileRepository.save(companyProfile, phoneNumbers, photos);
-
-    expect(companyProfile.cuit).toEqual(companyProfileData.cuit);
-    expect(companyProfile.companyName).toEqual(companyProfileData.companyName);
-    expect(companyProfile.slogan).toEqual(companyProfileData.slogan);
-    expect(companyProfile.description).toEqual(companyProfileData.description);
-    expect(companyProfile.logo).toEqual(companyProfileData.logo);
+    const { companyProfile, phoneNumbers, photos } = await createCompanyProfile();
+    expect(companyProfile.cuit).toEqual(companyProfileData().cuit);
+    expect(companyProfile.companyName).toEqual(companyProfileData().companyName);
+    expect(companyProfile.slogan).toEqual(companyProfileData().slogan);
+    expect(companyProfile.description).toEqual(companyProfileData().description);
+    expect(companyProfile.logo).toEqual(companyProfileData().logo);
     expect(companyProfile.phoneNumbers).toHaveLength(phoneNumbers.length);
     expect(companyProfile.photos).toHaveLength(photos.length);
   });
@@ -76,32 +90,7 @@ describe("CompanyProfileRepository", () => {
   });
 
   test("retrieve by id", async () => {
-    const companyProfileData = {
-      cuit: "30711819017",
-      companyName: "devartis",
-      slogan: "We craft web applications for great businesses",
-      description: "some description",
-      logo: "https://pbs.twimg.com/profile_images/1039514458282844161/apKQh1fu_400x400.jpg"
-    };
-    const companyProfile: CompanyProfile = new CompanyProfile(companyProfileData);
-    const phoneNumbers: CompanyProfilePhoneNumber[] = CompanyProfilePhoneNumberRepository.build(
-      [
-        43076555,
-        43076556,
-        43076455,
-        43076599
-      ]
-    );
-    const photos: CompanyProfilePhoto[] = CompanyProfilePhotoRepository.build(
-      [
-        `data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38G
-      IAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==`,
-        `data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38G
-      IAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==`
-      ]
-    );
-    await CompanyProfileRepository.save(companyProfile, phoneNumbers, photos);
-
+    const { companyProfile } = await createCompanyProfile();
     const expectedCompanyProfile = await CompanyProfileRepository.findById(companyProfile.id);
     expect(expectedCompanyProfile).not.toBeNull();
     expect(expectedCompanyProfile).not.toBeUndefined();
@@ -110,32 +99,7 @@ describe("CompanyProfileRepository", () => {
 
 
   test("retrieve all CompanyProfiles", async () => {
-    const companyProfileData = {
-      cuit: "30711819017",
-      companyName: "devartis",
-      slogan: "We craft web applications for great businesses",
-      description: "some description",
-      logo: "https://pbs.twimg.com/profile_images/1039514458282844161/apKQh1fu_400x400.jpg"
-    };
-    const companyProfile: CompanyProfile = new CompanyProfile(companyProfileData);
-    const phoneNumbers: CompanyProfilePhoneNumber[] = CompanyProfilePhoneNumberRepository.build(
-      [
-        43076555,
-        43076556,
-        43076455,
-        43076599
-      ]
-    );
-    const photos: CompanyProfilePhoto[] = CompanyProfilePhotoRepository.build(
-      [
-        `data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38G
-      IAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==`,
-        `data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38G
-      IAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==`
-      ]
-    );
-    await CompanyProfileRepository.save(companyProfile, phoneNumbers, photos);
-
+    const { companyProfile } = await createCompanyProfile();
     const expectedCompanyProfiles = await CompanyProfileRepository.findAll();
     expect(expectedCompanyProfiles).not.toBeNull();
     expect(expectedCompanyProfiles).not.toBeUndefined();
@@ -144,14 +108,7 @@ describe("CompanyProfileRepository", () => {
   });
 
   test("rollback transaction and raise error if photo is null", async () => {
-    const companyProfileData = {
-      cuit: "30711819017",
-      companyName: "devartis",
-      slogan: "We craft web applications for great businesses",
-      description: "some description",
-      logo: "https://pbs.twimg.com/profile_images/1039514458282844161/apKQh1fu_400x400.jpg"
-    };
-    const companyProfile: CompanyProfile = new CompanyProfile(companyProfileData);
+    const companyProfile: CompanyProfile = new CompanyProfile(companyProfileData());
     const photo: CompanyProfilePhoto = new CompanyProfilePhoto({ photo: null });
     await expect(CompanyProfileRepository.save(
       companyProfile, [],[photo]
@@ -164,23 +121,13 @@ describe("CompanyProfileRepository", () => {
   });
 
   test("rollback transaction and raise error if phoneNumber is null", async () => {
-    const companyProfileData = {
-      cuit: "30711819017",
-      companyName: "devartis",
-      slogan: "We craft web applications for great businesses",
-      description: "some description",
-      logo: "https://pbs.twimg.com/profile_images/1039514458282844161/apKQh1fu_400x400.jpg"
-    };
-    const companyProfile: CompanyProfile = new CompanyProfile(companyProfileData);
+    const companyProfile: CompanyProfile = new CompanyProfile(companyProfileData());
     const phoneNumber: CompanyProfilePhoneNumber = new CompanyProfilePhoneNumber(
-      {
-        phoneNumber: null
-      }
+      { phoneNumber: null }
     );
     await expect(CompanyProfileRepository.save(
       companyProfile, [phoneNumber]
     )).rejects.toThrow();
-
     const expectedCompanyProfiles = await CompanyProfileRepository.findAll();
     expect(expectedCompanyProfiles).not.toBeNull();
     expect(expectedCompanyProfiles).not.toBeUndefined();
