c041d0e39d12b70b083ebbc079a27567eae4a319
dylan <dylanalvarez1995@gmail.com>
2020-02-02 01:11:08 -0300

    Add company profile translations

--- src/models/Translation/defaultTranslations.ts
+++ src/models/Translation/defaultTranslations.ts
@@ -1,6 +1,12 @@
 export const defaultTranslations = {
   app: {
-    title: "Bolsa de trabajo"
+    title: "Bolsa de trabajo",
+    log_out: "Cerrar Sesión"
   },
-  my_company: "Mi empresa"
+  company_profile: {
+    title: "Perfil de mi empresa",
+    explanation: "Así se va a mostrar a los postulantes"
+  },
+  my_company: "Mi empresa",
+  edit: "Editar"
 };
