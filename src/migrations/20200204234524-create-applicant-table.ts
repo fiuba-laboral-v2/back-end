cb703cdac6ba5c668e8ace4eeddb0c80c047d6a7
llmanuel <llauromanuel@gmail.com>
2020-02-20 00:12:29 -0300

    Change applicant description to accept null

--- src/migrations/20200204234524-create-applicant-table.ts
+++ src/migrations/20200204234524-create-applicant-table.ts
@@ -25,7 +25,7 @@ export = {
         type: DataType.INTEGER
       },
       description: {
-        allowNull: true,
+        allowNull: false,
         type: DataType.TEXT
       },
       credits: {
