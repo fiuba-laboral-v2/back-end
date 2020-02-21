ab3360e6bf3020cd093898249619da874b0f4573
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-02-21 18:43:30 -0300

    Add option to findById in companyRepository to get associations

--- src/models/Company/Repository.ts
+++ src/models/Company/Repository.ts
@@ -62,9 +62,10 @@ export const CompanyRepository = {
     }
   },
   findById: async (id: number) => {
-    const company: Company | null = await Company.findOne(
-      { where: { id: id } }
-    );
+    const company: Company | null = await Company.findByPk(
+      id,
+      { include: [CompanyPhoneNumber, CompanyPhoto] }
+      );
     if (!company)  throw new CompanyNotFoundError(id);
     return company;
   },
