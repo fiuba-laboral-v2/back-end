4db1a460504f0b7779f8a6240249d12f7eb30f5a
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-02-21 22:37:16 -0300

    Add test to test Company associations

--- test/graphql/Company/queries/getCompanyById.test.ts
+++ test/graphql/Company/queries/getCompanyById.test.ts
@@ -1,44 +1,67 @@
 import { gql } from "apollo-server";
 import { executeQuery } from "../../ApolloTestClient";
 import { Company, CompanyRepository } from "../../../../src/models/Company";
+import { companyMockData, phoneNumbers, photos } from "../../../models/Company/mocks";
 import Database from "../../../../src/config/Database";
 
 const query = gql`
   query ($id: ID!) {
     getCompanyById(id: $id) {
-      cuit
-      companyName
+        cuit
+        companyName
+        slogan
+        description
+        logo
+        website
+        email
+        phoneNumbers
+        photos
     }
   }
 `;
 
-beforeAll(async () => {
-  await Database.setConnection();
-});
-
-beforeEach(async () => {
-  await CompanyRepository.truncate();
-});
-
-afterAll(async () => {
-  await Database.close();
-});
-
-test("find a company given its id", async () => {
-  const companyParams = { cuit: "30711819017", companyName: "devartis"};
-  const company: Company = await CompanyRepository.save(
-    new Company(companyParams)
-  );
-  const response = await executeQuery(query, { id: company.id });
-  expect(response.errors).toBeUndefined();
-  expect(response.data).not.toBeUndefined();
-  expect(response.data).toEqual({
-    getCompanyById: companyParams
+describe("getCompanyById", () => {
+  const companyCompleteData = {
+    ...companyMockData,
+    ...{ photos: photos, phoneNumbers: phoneNumbers }
+  };
+  beforeAll(async () => {
+    await Database.setConnection();
   });
-});
 
-test("returns error if the Company does not exists", async () => {
-  const notExistentId: number = 9999;
-  const response = await executeQuery(query, { id: notExistentId });
-  expect(response.errors).not.toBeUndefined();
+  beforeEach(async () => {
+    await CompanyRepository.truncate();
+  });
+
+  afterAll(async () => {
+    await Database.close();
+  });
+
+  it("finds a company given its id", async () => {
+    const company: Company = await CompanyRepository.create(
+      companyCompleteData
+    );
+    const response = await executeQuery(query, { id: company.id });
+    expect(response.errors).toBeUndefined();
+    expect(response.data).not.toBeUndefined();
+    expect(response.data).toEqual({
+      getCompanyById: companyCompleteData
+    });
+  });
+
+  it("returns error if the Company does not exists", async () => {
+    const notExistentId: number = 9999;
+    const response = await executeQuery(query, { id: notExistentId });
+    expect(response.errors).not.toBeUndefined();
+  });
+
+  it("find a company with photos with an empty array", async () => {
+    const company: Company = await CompanyRepository.create(
+      companyMockData
+    );
+    const response = await executeQuery(query, { id: company.id });
+    expect(response.errors).toBeUndefined();
+    expect(response.data).not.toBeUndefined();
+    expect(response.data.getCompanyById.photos).toHaveLength(0);
+  });
 });
