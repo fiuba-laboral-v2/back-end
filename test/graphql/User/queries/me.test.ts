385e33f4cfa8184ebd714f0a58cbdfde40803ac6
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-02-23 19:47:45 -0300

    Rename test me query for better redability

--- test/graphql/User/queries/me.test.ts
+++ test/graphql/User/queries/me.test.ts
@@ -4,7 +4,7 @@ import Database from "../../../../src/config/Database";
 import { UserRepository } from "../../../../src/models/User/Repository";
 import { User } from "../../../../src/models/User";
 
-const query = gql`
+const ME = gql`
   query {
     me {
       email
@@ -27,14 +27,14 @@ describe("Current User query", () => {
 
   it("returns current user if it's set in context", async () => {
     await UserRepository.create({ email: testCurrentUserEmail, password: "SomeCoolSecret123" });
-    const response = await executeQuery(query);
+    const response = await executeQuery(ME);
     expect(response.errors).toBeUndefined();
     expect(response.data).toEqual({ me: { email: testCurrentUserEmail } });
   });
 
   it("returns error if the current user is not set in context", async () => {
     const loggedIn = false;
-    const response = await executeQuery(query, {}, loggedIn);
+    const response = await executeQuery(ME, {}, loggedIn);
     expect(response.errors[0].message).toEqual("You are not authenticated");
   });
 });
