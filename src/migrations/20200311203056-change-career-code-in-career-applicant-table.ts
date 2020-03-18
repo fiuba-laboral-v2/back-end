adcd989fd3f38f38caaa62ce4f917d79aef97246
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-03-17 21:46:40 -0300

    Fix migration by using change colum  instead of removing it and creting it again

--- src/migrations/20200311203056-change-career-code-in-career-applicant-table.ts
+++ src/migrations/20200311203056-change-career-code-in-career-applicant-table.ts
@@ -3,38 +3,21 @@ import { STRING, INTEGER, QueryInterface } from "sequelize";
 
 export = {
   up: async (queryInterface: QueryInterface) => {
-    await queryInterface.removeColumn(
-      "CareersApplicants",
-      "careerCode"
-    );
-
-    return queryInterface.addColumn(
+    return queryInterface.changeColumn(
       "CareersApplicants",
       "careerCode",
       {
-        allowNull: false,
-        primaryKey: true,
-        type: STRING,
-        references: { model: "Careers", key: "code" }
+        type: STRING
       }
     );
   },
 
   down: async (queryInterface: QueryInterface) => {
-    await queryInterface.removeColumn(
-      "CareersApplicants",
-      "careerCode"
-    );
-
-    return queryInterface.addColumn(
+    return queryInterface.changeColumn(
       "CareersApplicants",
       "careerCode",
       {
-        allowNull: false,
-        primaryKey: true,
-        unique: true,
-        type: INTEGER,
-        references: { model: "Careers", key: "code" }
+        type: INTEGER
       }
     );
   }
