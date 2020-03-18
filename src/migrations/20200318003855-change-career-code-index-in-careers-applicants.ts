65ac16153d91a3888ca551b224bf7f9d083a519c
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-03-18 02:01:21 -0300

    Remove uniqe constrain

---
+++ src/migrations/20200318003855-change-career-code-index-in-careers-applicants.ts
@@ -0,0 +1,22 @@
+import { QueryInterface } from "sequelize";
+
+
+export = {
+  up: async (queryInterface: QueryInterface) => {
+    await queryInterface.removeConstraint(
+      "CareersApplicants",
+      "CareersApplicants_careerCode_key"
+    );
+  },
+
+  down: async (queryInterface: QueryInterface) => {
+    return queryInterface.addConstraint(
+      "CareersApplicants",
+      ["careerCode"],
+      {
+        type: "unique",
+        name: "CareersApplicants_careerCode_key"
+      }
+    );
+  }
+};
