1cd9288b4129f6797adffa0191cae94602e24175
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-03-20 21:45:41 -0300

    Remove unnecesary space

--- src/models/CareerApplicant/Repository.ts
+++ src/models/CareerApplicant/Repository.ts
@@ -18,7 +18,6 @@ export const CareerApplicantRepository = {
         transaction
       );
     }
-
     const careerApplicant = await CareerApplicantRepository.findByApplicantAndCareer(
       applicant.uuid,
       career.code
