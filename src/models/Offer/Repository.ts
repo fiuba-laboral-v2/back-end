9b9cb24e4332a752896c13c0cded6454db35d88b
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-04-05 23:36:43 -0300

    Rename create method of OfferRepository signature definition

--- src/models/Offer/Repository.ts
+++ src/models/Offer/Repository.ts
@@ -2,8 +2,8 @@ import { Offer, IOffer } from "./index";
 import { OfferNotFound } from "./Errors";
 
 export const OfferRepository = {
-  create: async (props: IOffer) => {
-    const company = new Offer({ ...props });
+  create: async (attributes: IOffer) => {
+    const company = new Offer({ ...attributes });
     return OfferRepository.save(company);
   },
   save: async (offer: Offer) => {
