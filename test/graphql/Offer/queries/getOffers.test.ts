0bc501929c4e4bb85ff3bae65b3ffcc4c02e9779
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-04-10 19:23:46 -0300

    Test getOffers query return data

--- test/graphql/Offer/queries/getOffers.test.ts
+++ test/graphql/Offer/queries/getOffers.test.ts
@@ -9,6 +9,7 @@ import { OfferRepository } from "../../../../src/models/Offer";
 import { careerMocks } from "../../../models/Career/mocks";
 import { companyMockData } from "../../../models/Company/mocks";
 import { OfferMocks } from "../../../models/Offer/mocks";
+import { omit, pick } from "lodash";
 
 const GET_OFFERS = gql`
     query {
@@ -48,13 +49,31 @@ describe("getOffers", () => {
   describe("when offers exists", () => {
     it("should return two offers when two offers exists", async () => {
       const { id } = await CompanyRepository.create(companyMockData);
-      const { code: careerCode1 } = await CareerRepository.create(careerMocks.careerData());
-      const { code: careerCode2 } = await CareerRepository.create(careerMocks.careerData());
-      await OfferRepository.create(OfferMocks.withOneCareer(id, careerCode1));
-      await OfferRepository.create(OfferMocks.withOneCareer(id, careerCode2));
+      const career1 = await CareerRepository.create(careerMocks.careerData());
+      const career2 = await CareerRepository.create(careerMocks.careerData());
+      const offerAttributes1 = OfferMocks.withOneCareer(id, career1.code);
+      const offerAttributes2 = OfferMocks.withOneCareer(id, career2.code);
+      await OfferRepository.create(offerAttributes1);
+      await OfferRepository.create(offerAttributes2);
       const { data: { getOffers }, errors } = await executeQuery(GET_OFFERS);
       expect(errors).toBeUndefined();
       expect(getOffers).toHaveLength(2);
+      expect(getOffers).toMatchObject(
+        [
+          omit(offerAttributes1, ["careers"]),
+          omit(offerAttributes2, ["careers"])
+        ]
+      );
+      expect(
+        getOffers
+          .map(offer => offer.careers)
+          .reduce((careers1, careers2) => careers1.concat(careers2))
+      ).toMatchObject(
+        [
+          pick(career1, ["code", "description", "credits"]),
+          pick(career2, ["code", "description", "credits"])
+        ]
+      );
     });
 
     it("should return no offers when no offers were created", async () => {
