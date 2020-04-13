ed2b798721d3269ab378cc39d36e78cdb5599553
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-04-13 17:09:39 -0300

    Remove unused getApplicantByPadron query

--- src/graphql/Applicant/Queries/index.ts
+++ src/graphql/Applicant/Queries/index.ts
@@ -1,10 +1,8 @@
 import { getApplicant } from "./getApplicant";
-import { getApplicantByPadron } from "./getApplicantByPadron";
 import { getApplicants } from "./getApplicants";
 
 const applicantQueries = {
   getApplicant,
-  getApplicantByPadron,
   getApplicants
 };
 
