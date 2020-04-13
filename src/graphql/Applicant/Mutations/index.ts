6f4cac37f5ae4390ce1a13dc173811df1a00bf1f
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-04-13 17:14:26 -0300

    Remove unused mutations

--- src/graphql/Applicant/Mutations/index.ts
+++ src/graphql/Applicant/Mutations/index.ts
@@ -1,17 +1,9 @@
 import { saveApplicant } from "./saveApplicant";
 import { updateApplicant } from "./updateApplicant";
-import { deleteApplicantCapabilities } from "./deleteApplicantCapabilities";
-import { deleteApplicantCareers } from "./deleteApplicantCareers";
-import { deleteSection } from "./deleteSection";
-import { deleteLink } from "./deleteLink";
 
 const applicantMutations = {
   saveApplicant,
-  updateApplicant,
-  deleteApplicantCapabilities,
-  deleteApplicantCareers,
-  deleteSection,
-  deleteLink
+  updateApplicant
 };
 
 export { applicantMutations };
