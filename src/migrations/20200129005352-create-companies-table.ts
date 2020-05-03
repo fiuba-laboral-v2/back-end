3933175b43cb6b320b52ec02b1054d8b1393f2f8
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-05-02 21:24:17 -0300

    Add cuit contraint

--- src/migrations/20200129005352-create-companies-table.ts
+++ src/migrations/20200129005352-create-companies-table.ts
@@ -3,45 +3,59 @@ import uuid from "uuid/v4";
 
 export = {
   up: (queryInterface: QueryInterface) => {
-    return queryInterface.createTable("Companies", {
-      uuid: {
-        allowNull: false,
-        primaryKey: true,
-        type: UUID,
-        defaultValue: uuid()
-      },
-      cuit: {
-        allowNull: false,
-        type: STRING,
-        unique: true
-      },
-      companyName: {
-        allowNull: false,
-        type: STRING
-      },
-      slogan: {
-        type: STRING
-      },
-      description: {
-        type: STRING
-      },
-      logo: {
-        type: TEXT
-      },
-      website: {
-        type: STRING
-      },
-      email: {
-        type: STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: DATE
-      }
+    return queryInterface.sequelize.transaction(async transaction => {
+      await queryInterface.createTable(
+        "Companies",
+        {
+          uuid: {
+            allowNull: false,
+            primaryKey: true,
+            type: UUID,
+            defaultValue: uuid()
+          },
+          cuit: {
+            allowNull: false,
+            type: STRING
+          },
+          companyName: {
+            allowNull: false,
+            type: STRING
+          },
+          slogan: {
+            type: STRING
+          },
+          description: {
+            type: STRING
+          },
+          logo: {
+            type: TEXT
+          },
+          website: {
+            type: STRING
+          },
+          email: {
+            type: STRING
+          },
+          createdAt: {
+            allowNull: false,
+            type: DATE
+          },
+          updatedAt: {
+            allowNull: false,
+            type: DATE
+          }
+        },
+        { transaction }
+      );
+      await queryInterface.addConstraint(
+        "Companies",
+        ["cuit"],
+        {
+          type: "unique",
+          name: "Companies_cuit_key",
+          transaction
+        }
+      );
     });
   },
   down: (queryInterface: QueryInterface) => {
