3b92f5e2e0871c12fe4942d8746f869d74bbccbc
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-05-04 23:34:49 -0300

    Add uuid to IUser interface

--- src/models/User/Interface.ts
+++ src/models/User/Interface.ts
@@ -1,4 +1,5 @@
 export interface IUser {
+  uuid?: string;
   name: string;
   surname: string;
   email: string;
@@ -6,6 +7,7 @@ export interface IUser {
 }
 
 export interface IUserEditable {
+  uuid?: string;
   name?: string;
   surname?: string;
 }
