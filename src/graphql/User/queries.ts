82c3da273f86e7fb897f1fe4ffea343f8e277536
llmanuel <llauromanuel@gmail.com>
2020-05-10 16:54:38 -0300

    refactor

--- src/graphql/User/queries.ts
+++ src/graphql/User/queries.ts
@@ -5,8 +5,7 @@ import { UserRepository } from "../../models/User/Repository";
 export const userQueries = {
   me: {
     type: GraphQLUser,
-    resolve: async (_: undefined, __: object, { currentUser }: IApolloServerContext) => {
-      return UserRepository.findByEmail(currentUser!.email);
-    }
+    resolve: async (_: undefined, __: object, { currentUser }: IApolloServerContext) =>
+      UserRepository.findByEmail(currentUser!.email)
   }
 };
