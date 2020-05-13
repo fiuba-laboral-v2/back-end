ca0ff93cf2b205d5a1c6312fa16566ef25fee50b
Dylan Alvarez <dalvarez@devartis.com>
2020-05-13 17:25:19 -0300

    Add translations to create offer page

--- src/models/Translation/defaultTranslations.ts
+++ src/models/Translation/defaultTranslations.ts
@@ -37,6 +37,15 @@ export const defaultTranslations = {
     { key: "career", value: "Carrera" },
     { key: "credits", value: "Créditos aprobados" }
   ],
+  createOffer: [
+    { key: "formTitle", value: "Publicar oferta" },
+    { key: "offerTitle", value: "Título" },
+    { key: "description", value: "Descripción" },
+    { key: "hoursPerDay", value: "Horas por día" },
+    { key: "minimumSalary", value: "Salario mínimo" },
+    { key: "maximumSalary", value: "Salario máximo" },
+    { key: "submit", value: "Publicar" }
+  ],
   companySignUp: [
     { key: "title", value: "Crear tu cuenta" },
     { key: "email", value: "Email" },
