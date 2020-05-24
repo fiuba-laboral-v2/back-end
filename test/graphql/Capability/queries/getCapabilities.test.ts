1705a8a052b71ab8d59cecd3ef10dbf1bd4a46d1
llmanuel <llauromanuel@gmail.com>
2020-05-24 16:02:40 -0300

    Add getCapabilities permission test

--- test/graphql/Capability/queries/getCapabilities.test.ts
+++ test/graphql/Capability/queries/getCapabilities.test.ts
@@ -1,7 +1,11 @@
 import { gql } from "apollo-server";
 import Database from "../../../../src/config/Database";
 import { CapabilityRepository } from "../../../../src/models/Capability";
-import { executeQuery } from "../../ApolloTestClient";
+import { client } from "../../ApolloTestClient";
+
+import { testClientFactory } from "../../../mocks/testClientFactory";
+
+import { AuthenticationError } from "../../../../src/graphql/Errors";
 
 const GET_CAPABILITIES = gql`
     query {
@@ -20,12 +24,13 @@ describe("getCapabilities", () => {
   afterAll(() => Database.close());
 
   it("brings all capabilities in the database", async () => {
+    const { apolloClient } = await testClientFactory.user();
     const [java, python, ruby] = await Promise.all(
       ["java", "python", "ruby"].map(description =>
         CapabilityRepository.create({ description: description })
       )
     );
-    const { data } = await executeQuery(GET_CAPABILITIES);
+    const { data } = await apolloClient.query({ query: GET_CAPABILITIES });
     expect(data!.getCapabilities).toEqual(
       expect.arrayContaining(
         [
@@ -35,5 +40,14 @@ describe("getCapabilities", () => {
         ]
       )
     );
+  });
+
+  describe("Errors", () => {
+    it("returns an error if there is no current user", async () => {
+      const apolloClient = client.loggedOut;
+
+      const { errors } = await apolloClient.query({ query: GET_CAPABILITIES });
+      expect(errors![0].extensions!.data).toEqual({ errorType: AuthenticationError.name });
+    });
   });
 });
