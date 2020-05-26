7fa9533324f3c369ae2c11737fe072e88ed9116a
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-05-26 00:39:24 -0300

    Add updateCurrentCompany mutation

---
+++ test/graphql/Company/mutations/updateCurrentCompany.test.ts
@@ -0,0 +1,108 @@
+import { gql } from "apollo-server";
+import Database from "../../../../src/config/Database";
+
+import { testClientFactory } from "../../../mocks/testClientFactory";
+
+import { CompanyRepository } from "../../../../src/models/Company";
+import { UserRepository } from "../../../../src/models/User";
+
+import { client } from "../../ApolloTestClient";
+import { AuthenticationError, UnauthorizedError } from "../../../../src/graphql/Errors";
+
+const UPDATE_CURRENT_COMPANY = gql`
+    mutation (
+        $cuit: String,
+        $companyName: String,
+        $slogan: String,
+        $description: String,
+        $logo: String,
+        $website: String,
+        $email: String,
+        $phoneNumbers: [String],
+        $photos: [String]) {
+        updateCurrentCompany(
+            cuit: $cuit,
+            companyName: $companyName,
+            slogan: $slogan, description: $description,
+            logo: $logo,
+            website: $website,
+            email: $email,
+            phoneNumbers: $phoneNumbers,
+            photos: $photos
+        ) {
+            cuit
+            companyName
+            slogan
+            description
+            logo
+            website
+            email
+            phoneNumbers
+            photos
+        }
+    }
+`;
+
+describe("updateCurrentCompany", () => {
+  beforeAll(async () => {
+    Database.setConnection();
+    await CompanyRepository.truncate();
+    await UserRepository.truncate();
+  });
+
+  afterAll(async () => {
+    await Database.close();
+    await CompanyRepository.truncate();
+    await UserRepository.truncate();
+  });
+
+  describe("When the update succeeds", () => {
+    it("update all company attributes", async () => {
+      const { apolloClient } = await testClientFactory.company();
+      const dataToUpdate = {
+        cuit: "30711311773",
+        companyName: "Devartis SA",
+        slogan: "new slogan",
+        description: "new description",
+        logo: "",
+        website: "http://www.new-site-com",
+        email: "old@devartis.com",
+        phoneNumbers: ["1159871234", "1160393692"],
+        photos: [
+          "data:image/jpeg;base64,/9j/4AAQSkZJRgABA AgICAgICAgICAgICAgIA==",
+          "data:image/jpeg;base64,/9j/4AAQSkZJRgABA AICAgICAgIA=="
+        ]
+      };
+      const { data, errors } = await apolloClient.mutate({
+        mutation: UPDATE_CURRENT_COMPANY,
+        variables: dataToUpdate
+      });
+      expect(errors).toBeUndefined();
+      expect(data!.updateCurrentCompany).toEqual(dataToUpdate);
+    });
+  });
+
+  describe("when the update errors", () => {
+    it("throws an error if there is no current user", async () => {
+      const apolloClient = client.loggedOut;
+      const dataToUpdate = { companyName: "new company name" };
+      const { errors } = await apolloClient.mutate({
+        mutation: UPDATE_CURRENT_COMPANY,
+        variables: dataToUpdate
+      });
+
+      expect(errors![0].extensions!.data).toEqual({ errorType: AuthenticationError.name });
+    });
+
+    it("throws an error if current user is not a company user", async () => {
+      const { apolloClient } = await testClientFactory.user();
+      const dataToUpdate = { companyName: "new company name" };
+      const { errors } = await apolloClient.mutate({
+        mutation: UPDATE_CURRENT_COMPANY,
+        variables: dataToUpdate
+      });
+
+      expect(errors![0].extensions!.data).toEqual({ errorType: UnauthorizedError.name });
+    });
+  });
+});
