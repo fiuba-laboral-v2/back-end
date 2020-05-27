c136ca819a129299abc929893ffa9d0fd0efa380
Dylan Alvarez <dalvarez@devartis.com>
2020-05-26 21:42:29 -0300

    Make navbar translation coincide with page title

--- src/models/Translation/defaultTranslations.ts
+++ src/models/Translation/defaultTranslations.ts
@@ -108,7 +108,7 @@ export const defaultTranslations = {
     { key: "companies", value: "Empresas" },
     { key: "jobOffers", value: "Ofertas de trabajo" },
     { key: "jobApplications", value: "Postulaciones" },
-    { key: "createOffer", value: "Crear Oferta" },
+    { key: "createOffer", value: "Publicar Oferta" },
     { key: "signUp", value: "Crear tu cuenta" },
     { key: "logIn", value: "Iniciar sesión" },
     { key: "logOut", value: "Cerrar sesión" },
