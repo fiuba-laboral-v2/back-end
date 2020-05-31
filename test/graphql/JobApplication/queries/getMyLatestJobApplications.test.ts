d0cd952a3fa5a029fd96e4d182ac3401952461bf
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-05-31 00:53:33 -0300

    Use offer generator in getMyLatestJobApplications query test

--- test/graphql/JobApplication/queries/getMyLatestJobApplications.test.ts
+++ test/graphql/JobApplication/queries/getMyLatestJobApplications.test.ts
@@ -5,12 +5,11 @@ import Database from "../../../../src/config/Database";
 import { UserRepository } from "../../../../src/models/User";
 import { Applicant } from "../../../../src/models/Applicant";
 import { CompanyRepository } from "../../../../src/models/Company";
-import { OfferRepository } from "../../../../src/models/Offer";
 import { JobApplicationRepository } from "../../../../src/models/JobApplication";
 
 import { AuthenticationError, UnauthorizedError } from "../../../../src/graphql/Errors";
 
-import { OfferMocks } from "../../../models/Offer/mocks";
+import { OfferGenerator, TOfferGenerator } from "../../../generators/Offer";
 import { ApplicantGenerator } from "../../../generators/Applicant";
 import { testClientFactory } from "../../../mocks/testClientFactory";
 
@@ -35,12 +34,14 @@ const GET_MY_LATEST_JOB_APPLICATIONS = gql`
 
 describe("getMyLatestJobApplications", () => {
   let applicant: Applicant;
+  let offers: TOfferGenerator;
 
   beforeAll(async () => {
     Database.setConnection();
     await UserRepository.truncate();
     await CompanyRepository.truncate();
     applicant = await ApplicantGenerator.withMinimumData().next().value;
+    offers = await OfferGenerator.model.withObligatoryData();
   });
 
   afterAll(() => Database.close());
@@ -48,7 +49,7 @@ describe("getMyLatestJobApplications", () => {
   describe("when the input is valid", () => {
     it("returns all my company jobApplications", async () => {
       const { company, apolloClient } = await testClientFactory.company();
-      const offer = await OfferRepository.create(OfferMocks.completeData(company.uuid));
+      const offer = await offers.next({ companyUuid: company.uuid }).value;
       const jobApplication = await JobApplicationRepository.apply(applicant.uuid, offer);
 
       const { data, errors } = await apolloClient.query({
