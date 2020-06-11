a81e8273736afec118b2f2e44dbc03edd4359b8f
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-06-11 01:30:31 -0300

    Add unique userUuid constraint to Admins table

--- src/migrations/20200609181509-create-admins-table.ts
+++ src/migrations/20200609181509-create-admins-table.ts
@@ -2,30 +2,42 @@ import { DATE, UUID, QueryInterface } from "sequelize";
 
 export = {
   up: (queryInterface: QueryInterface) =>
-    queryInterface.createTable(
-      "Admins",
-      {
-        uuid: {
-          allowNull: false,
-          primaryKey: true,
-          type: UUID
+    queryInterface.sequelize.transaction(async transaction => {
+      await queryInterface.createTable(
+        "Admins",
+        {
+          uuid: {
+            allowNull: false,
+            primaryKey: true,
+            type: UUID
+          },
+          userUuid: {
+            allowNull: false,
+            references: { model: "Users", key: "uuid" },
+            onDelete: "CASCADE",
+            type: UUID
+          },
+          createdAt: {
+            allowNull: false,
+            type: DATE
+          },
+          updatedAt: {
+            allowNull: false,
+            type: DATE
+          }
         },
-        userUuid: {
-          allowNull: false,
-          references: { model: "Users", key: "uuid" },
-          onDelete: "CASCADE",
-          type: UUID
-        },
-        createdAt: {
-          allowNull: false,
-          type: DATE
-        },
-        updatedAt: {
-          allowNull: false,
-          type: DATE
+        { transaction }
+      );
+      await queryInterface.addConstraint(
+        "Admins",
+        ["userUuid"],
+        {
+          type: "unique",
+          name: "Admins_userUuid_key",
+          transaction
         }
-      }
-    ),
+      );
+    }),
   down: (queryInterface: QueryInterface) =>
     queryInterface.dropTable("Admins")
 };
