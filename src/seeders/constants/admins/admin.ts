3b3d57a08024eec19acf46b552e770c5f002350f
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-06-12 00:32:11 -0300

    Add admin user to seed data

---
+++ src/seeders/constants/admins/admin.ts
@@ -0,0 +1,19 @@
+import { uuids } from "../uuids";
+import { hashSync } from "bcrypt";
+
+export const admin = {
+  user: {
+    uuid: uuids.admin.user,
+    email: "admin@admin",
+    password: hashSync("SecurePassword1010", 10),
+    name: "admin",
+    surname: "admin",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  admin: {
+    userUuid: uuids.admin.user,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
+};
