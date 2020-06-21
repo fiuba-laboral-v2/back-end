0980dadb386231a5cc5bb4e82b9061ed4d86bfa6
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-06-20 23:53:50 -0300

    Rename method in isApprovedCompany permission rule from companyHasApprovedStatus to companyIsApproved

--- src/graphql/Rules/isApprovedCompany.ts
+++ src/graphql/Rules/isApprovedCompany.ts
@@ -5,11 +5,11 @@ import { ApprovalStatus } from "../../models/ApprovalStatus";
 import { isCompanyUser } from "./isCompanyUser";
 import { CompanyRepository } from "../../models/Company";
 
-const companyHasApprovedStatus = rule({ cache: "contextual" })
+const companyIsApproved = rule({ cache: "contextual" })
   (async (parent, args, { currentUser }: { currentUser: ICompanyUser }) => {
     const company = await CompanyRepository.findByUuid(currentUser.company.uuid);
     if (company.approvalStatus !== ApprovalStatus.approved) throw new UnauthorizedError();
     return true;
   });
 
-export const isApprovedCompany = chain(isCompanyUser, companyHasApprovedStatus);
+export const isApprovedCompany = chain(isCompanyUser, companyIsApproved);
