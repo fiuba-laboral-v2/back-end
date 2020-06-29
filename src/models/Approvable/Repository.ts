3cc9e6c8abd197bab2971423f0976b7a4f222b95
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-06-28 23:30:43 -0300

    Create approval_status enum type; Adapt Companies's and Applicants's ApprovalStatus column to approval_status type; Refactor findPending method in ApprovableRepository to use FULL OUTER JOIN

--- src/models/Approvable/Repository.ts
+++ src/models/Approvable/Repository.ts
@@ -1,10 +1,12 @@
 import Database from "../../config/Database";
 import { Company } from "../Company";
+import { Applicant } from "../Applicant";
 import { find } from "lodash";
 import { ApprovalStatus } from "../ApprovalStatus";
 
 const APPROVABLE_MODELS = [
-  Company
+  Company,
+  Applicant
 ];
 
 const TABLE_NAME_COLUMN = "tableNameColumn";
@@ -12,12 +14,55 @@ const TABLE_NAME_COLUMN = "tableNameColumn";
 const getModelByTableName = (tableName: string) =>
   find(APPROVABLE_MODELS, ["tableName", tableName]);
 
-const findPendingQuery = () => `
-  SELECT *, '${Company.tableName}' AS "${TABLE_NAME_COLUMN}"
-  FROM "${Company.tableName}"
-  WHERE "approvalStatus" = '${ApprovalStatus.pending}'
-  ORDER BY "updatedAt" DESC
+const getTablesByColumn = () => {
+  const tablesByColumn = {};
+  APPROVABLE_MODELS.forEach(model => {
+    const columnNames = Object.keys(model.rawAttributes).concat([TABLE_NAME_COLUMN]);
+    columnNames.forEach(columnName => {
+      const tableNames = tablesByColumn[columnName] || [];
+      tableNames.push(model.tableName);
+      tablesByColumn[columnName] = tableNames;
+    });
+  });
+  return tablesByColumn;
+};
+
+const getRowsToSelect = () => {
+  const tablesByColumn: object = getTablesByColumn();
+  return Object.entries(tablesByColumn).map(([columnName, tableNames]) =>
+    `COALESCE (
+      ${tableNames.map(tableName => `${tableName}."${columnName}"`).join(",")}
+    ) AS "${columnName}"`
+  ).join(",");
+};
+
+const getFullOuterJoin = () => {
+  let selectStatements = APPROVABLE_MODELS.map(model => {
+    const tableName = model.tableName;
+    return `(
+      SELECT *, '${tableName}' AS "${TABLE_NAME_COLUMN}" FROM "${tableName}"
+    ) AS ${tableName}`;
+  });
+  selectStatements = selectStatements.map((selectStatement, index) => {
+    if (index === 0) return selectStatement;
+    return `${selectStatement} ON FALSE`;
+  });
+  return selectStatements.join(" FULL OUTER JOIN ");
+};
+
+const findApprovablesQuery = () => `
+  SELECT ${getRowsToSelect()}
+  FROM (${getFullOuterJoin()})
 `;
+
+const findPendingQuery = () => {
+  return `
+    WITH "Approvable" AS (${findApprovablesQuery()})
+    SELECT * FROM "Approvable"
+    WHERE "Approvable"."approvalStatus" = '${ApprovalStatus.pending}'
+    ORDER BY "Approvable"."updatedAt" DESC
+  `;
+};
 
 export const ApprovableRepository = {
   findPending: async () => {
