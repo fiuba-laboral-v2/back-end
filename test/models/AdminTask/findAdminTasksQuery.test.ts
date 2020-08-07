91bfdc64a5633fb04f9afc3e8dd170697dbc839c
Dylan Alvarez <dalvarez@devartis.com>
2020-08-07 04:13:47 -0300

    Add test for updatedBeforeThan in findAdminTasksQuery

--- test/models/AdminTask/findAdminTasksQuery.test.ts
+++ test/models/AdminTask/findAdminTasksQuery.test.ts
@@ -214,6 +214,52 @@ describe("findAdminTasksQuery", () => {
     expect(query).toEqualIgnoringSpacing(expectedQuery);
   });
 
+  it("optionally filters by maximum updatedAt (not inclusive)", async () => {
+    const limit = 205;
+    const query = findAdminTasksQuery({
+      adminTaskTypes: [AdminTaskType.Applicant, AdminTaskType.Company],
+      statuses: [ApprovalStatus.pending, ApprovalStatus.approved, ApprovalStatus.rejected],
+      updatedBeforeThan: new Date("1995-12-17T03:24:00Z"),
+      limit
+    });
+    const expectedQuery = `
+      WITH "AdminTask" AS
+        (
+          SELECT
+            COALESCE(Companies."uuid",Applicants."uuid") AS "uuid",
+            COALESCE(Companies."cuit") AS "cuit",
+            COALESCE(Companies."companyName") AS "companyName",
+            COALESCE(Companies."slogan") AS "slogan",
+            COALESCE(Companies."description",Applicants."description") AS "description",
+            COALESCE(Companies."logo") AS "logo",
+            COALESCE(Companies."website") AS "website",
+            COALESCE(Companies."email") AS "email",
+            COALESCE(Companies."approvalStatus",Applicants."approvalStatus") AS "approvalStatus",
+            COALESCE(Companies."createdAt",Applicants."createdAt") AS "createdAt",
+            COALESCE(Companies."updatedAt",Applicants."updatedAt") AS "updatedAt",
+            COALESCE(Companies."tableNameColumn",Applicants."tableNameColumn") AS "tableNameColumn",
+            COALESCE(Applicants."padron") AS "padron",
+            COALESCE(Applicants."userUuid") AS "userUuid"
+          FROM (
+            (SELECT *, 'Companies' AS "tableNameColumn" FROM "Companies") AS Companies
+            FULL OUTER JOIN
+            (SELECT *, 'Applicants' AS "tableNameColumn" FROM "Applicants") AS Applicants ON FALSE
+          )
+        )
+      SELECT * FROM "AdminTask"
+      WHERE (
+        "AdminTask"."approvalStatus" = 'pending'
+        OR "AdminTask"."approvalStatus" = 'approved'
+        OR "AdminTask"."approvalStatus" = 'rejected'
+      ) AND (
+        "AdminTask"."updatedAt" < '1995-12-17T03:24:00.000Z'
+      )
+      ORDER BY "AdminTask"."updatedAt" DESC
+      LIMIT ${limit}
+    `;
+    expect(query).toEqualIgnoringSpacing(expectedQuery);
+  });
+
   it("returns an sql query of adminTasks in approved and rejected statuses", async () => {
     const limit = 210;
     const query = findAdminTasksQuery({
