9924321aacb8eb290d6a0129d146396cec07a6f5
Dylan Alvarez <dalvarez@devartis.com>
2020-08-08 14:47:05 -0300

    Instantiate admin once in test instead of many times

--- test/graphql/Applicant/queries/getApplicants.test.ts
+++ test/graphql/Applicant/queries/getApplicants.test.ts
@@ -12,6 +12,7 @@ import { TestClientGenerator } from "$generators/TestClient";
 import { AdminGenerator } from "$generators/Admin";
 import { ApplicantGenerator } from "$generators/Applicant";
 import { Secretary } from "$models/Admin";
+import { Admin } from "$models";
 
 const GET_APPLICANTS = gql`
     query getApplicants {
@@ -48,11 +49,13 @@ const GET_APPLICANTS = gql`
 
 describe("getApplicants", () => {
   let careers: TCareerGenerator;
+  let admin: Admin;
 
   beforeAll(async () => {
     await CareerRepository.truncate();
     await UserRepository.truncate();
     careers = CareerGenerator.instance();
+    admin = await AdminGenerator.instance(Secretary.extension);
   });
 
   describe("when no applicant exists", () => {
@@ -74,10 +77,7 @@ describe("getApplicants", () => {
         apolloClient
       } = await TestClientGenerator.applicant({
         careers: applicantCareer,
-        status: {
-          approvalStatus: ApprovalStatus.approved,
-          admin: await AdminGenerator.instance(Secretary.extension)
-        }
+        status: { approvalStatus: ApprovalStatus.approved, admin }
       });
 
       const { data, errors } = await apolloClient.query({ query: GET_APPLICANTS });
@@ -114,10 +114,7 @@ describe("getApplicants", () => {
       } = await TestClientGenerator.applicant({
         careers: applicantCareersData,
         capabilities: ["Go"],
-        status: {
-          approvalStatus: ApprovalStatus.approved,
-          admin: await AdminGenerator.instance(Secretary.extension)
-        }
+        status: { approvalStatus: ApprovalStatus.approved, admin }
       });
       const secondApplicant = await ApplicantGenerator.instance.withMinimumData({
         careers: applicantCareersData,
@@ -179,10 +176,7 @@ describe("getApplicants", () => {
 
     it("returns an error if current user is rejected applicant", async () => {
       const { apolloClient } = await TestClientGenerator.applicant({
-        status: {
-          approvalStatus: ApprovalStatus.rejected,
-          admin: await AdminGenerator.instance(Secretary.extension)
-        }
+        status: { approvalStatus: ApprovalStatus.rejected, admin }
       });
       const { errors } = await apolloClient.query({ query: GET_APPLICANTS });
       expect(errors![0].extensions!.data).toEqual({ errorType: UnauthorizedError.name });
