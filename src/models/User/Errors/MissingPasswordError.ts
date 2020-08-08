4b8e616a83e16bfd6712d41d691ae14a06e58095
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-08-07 21:12:28 -0300

    Remove unused MissingPasswordError

--- src/models/User/Errors/MissingPasswordError.ts
+++
@@ -1,9 +0,0 @@
-export class MissingPasswordError extends Error {
-  public static buildMessage() {
-    return "Password must be given to authenticate";
-  }
-
-  constructor() {
-    super(MissingPasswordError.buildMessage());
-  }
-}
--- src/models/User/Errors/index.ts
+++ src/models/User/Errors/index.ts
@@ -1,3 +1,2 @@
 export { UserNotFoundError } from "./UserNotFoundError";
 export { MissingDniError } from "./MissingDniError";
-export { MissingPasswordError } from "./MissingPasswordError";
