bc5fe72cae8a0bcd1042263bb207bba6cf32be0c
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-08-09 17:12:59 -0300

    Remove unused Optional SequelizeModelValidator

--- src/models/SequelizeModelValidators/index.ts
+++ src/models/SequelizeModelValidators/index.ts
@@ -1,4 +1,3 @@
 export { isUuid } from "./isUuid";
 export { isApprovalStatus } from "./isApprovalStatus";
 export { isSecretary } from "./isSecretary";
-export { optional } from "./optional";
--- src/models/SequelizeModelValidators/optional.ts
+++
@@ -1,5 +0,0 @@
-export const optional = <T>(validate: (attribute: T) => void) => ({
-  validate: {
-    isOptional: (attribute?: T) => attribute === undefined || validate(attribute)
-  }
-});
