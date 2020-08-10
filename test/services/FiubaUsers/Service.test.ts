0588b7e1d4ac9a74712857f834deea68fcdc240c
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-08-09 21:50:30 -0300

    Test FiubaUsersService authenticate to always return true for development and test_travis environments

--- test/services/FiubaUsers/Service.test.ts
+++ test/services/FiubaUsers/Service.test.ts
@@ -3,9 +3,19 @@ import {
   InvalidEmptyPasswordError,
   InvalidEmptyUsernameError
 } from "$services/FiubaUsers";
+import { Environment } from "$config/Environment";
 import { DniGenerator } from "$generators/DNI";
 
 describe("FiubaUsersService", () => {
+  const expectToReturnTrueForEnvironment = async (environment: string) => {
+    Object.defineProperty(Environment, "NODE_ENV", {
+      get: () => environment
+    });
+    expect(
+      await FiubaUsersService.authenticate({ dni: DniGenerator.generate(), password: "password" })
+    ).toBe(true);
+  };
+
   it("throws an error if the username is empty", async () => {
     await expect(
       FiubaUsersService.authenticate({
@@ -34,5 +44,13 @@ describe("FiubaUsersService", () => {
     expect(
       await FiubaUsersService.authenticate({ dni: DniGenerator.generate(), password: "password" })
     ).toBe(true);
+  });
+
+  it("always returns true in the test_travis environment", async () => {
+    await expectToReturnTrueForEnvironment(Environment.TEST_TRAVIS);
+  });
+
+  it("always returns true in the development environment", async () => {
+    await expectToReturnTrueForEnvironment(Environment.DEVELOPMENT);
   });
 });
