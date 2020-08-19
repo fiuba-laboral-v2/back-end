f2a45211f211911d8c213380534c92265783a3dc
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-08-19 02:45:23 -0300

    Add seeders data

--- src/seeders/constants/offers/devartis/offers/index.ts
+++ src/seeders/constants/offers/devartis/offers/index.ts
@@ -2,5 +2,6 @@ import { javaJunior } from "./javaJunior";
 import { javaSemiSenior } from "./javaSemiSenior";
 import { rubySenior } from "./rubySenior";
 import { rubyJunior } from "./rubyJunior";
+import { rubySemiSenior } from "./rubySemiSenior";
 
-export const devartisOffers = [javaJunior, javaSemiSenior, rubySenior, rubyJunior];
+export const devartisOffers = [javaJunior, javaSemiSenior, rubySenior, rubyJunior, rubySemiSenior];
