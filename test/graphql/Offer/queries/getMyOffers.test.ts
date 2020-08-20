bc03e1ae6be8ee8c0bd5e384c662525b84fdc601
Dylan Alvarez <dalvarez@devartis.com>
2020-08-20 04:21:47 -0300

    Add pagination tests to 'My Offers' list

--- test/graphql/Offer/queries/getMyOffers.test.ts
+++ test/graphql/Offer/queries/getMyOffers.test.ts
@@ -4,7 +4,7 @@ import { client } from "../../ApolloTestClient";
 import { AuthenticationError, UnauthorizedError } from "$graphql/Errors";
 
 import { ApprovalStatus } from "$models/ApprovalStatus";
-import { Admin } from "$models";
+import { Admin, Offer } from "$models";
 import { CareerRepository } from "$models/Career";
 import { CompanyRepository } from "$models/Company";
 import { UserRepository } from "$models/User";
@@ -16,6 +16,8 @@ import { OfferGenerator } from "$generators/Offer";
 import { AdminGenerator } from "$generators/Admin";
 import { TestClientGenerator } from "$generators/TestClient";
 import { Secretary } from "$models/Admin";
+import { range } from "lodash";
+import { mockItemsPerPage } from "$mocks/config/PaginationConfig";
 
 const GET_MY_OFFERS = gql`
   query getMyOffers($updatedBeforeThan: PaginatedInput) {
@@ -38,9 +40,11 @@ describe("getMyOffers", () => {
     admin = await AdminGenerator.instance({ secretary: Secretary.extension });
   });
 
-  describe("when offers exists", () => {
+  describe("when offers exist", () => {
     let offer1;
     let offer2;
+    let apolloClient;
+    let company;
 
     const createOffers = async (companyUuid: string) => {
       const { uuid } = await CompanyGenerator.instance.withMinimumData();
@@ -60,13 +64,18 @@ describe("getMyOffers", () => {
       });
     };
 
-    it("returns only the offers that the company made, by updatedAt desc", async () => {
-      const { apolloClient, company } = await TestClientGenerator.company({
+    beforeAll(async () => {
+      const companyClient = await TestClientGenerator.company({
         status: {
           admin,
           approvalStatus: ApprovalStatus.approved
         }
       });
+      apolloClient = companyClient.apolloClient;
+      company = companyClient.company;
+    });
+
+    it("returns only the offers that the company made, by updatedAt desc", async () => {
       await createOffers(company.uuid);
       const { data, errors } = await apolloClient.query({
         query: GET_MY_OFFERS
@@ -78,6 +87,55 @@ describe("getMyOffers", () => {
         { uuid: offer2.uuid },
         { uuid: offer1.uuid }
       ]);
+    });
+
+    describe("pagination", () => {
+      let newOffersByDescUpdatedAt: Offer[] = [];
+
+      beforeAll(async () => {
+        for (const _ of range(15)) {
+          newOffersByDescUpdatedAt.push(
+            await OfferRepository.create(
+              OfferGenerator.data.withObligatoryData({
+                companyUuid: company.uuid
+              })
+            )
+          );
+        }
+        newOffersByDescUpdatedAt = newOffersByDescUpdatedAt.sort(offer => -offer.updatedAt);
+      });
+
+      it("gets the latest 10 offers", async () => {
+        const itemsPerPage = 10;
+        mockItemsPerPage(itemsPerPage);
+        const { data } = await apolloClient.query({ query: GET_MY_OFFERS });
+        expect(data!.getMyOffers.results.map(offer => offer.uuid)).toEqual(
+          newOffersByDescUpdatedAt.slice(0, itemsPerPage).map(offer => offer.uuid)
+        );
+        expect(data!.getMyOffers.shouldFetchMore).toEqual(true);
+      });
+
+      it("gets the next 3 offers", async () => {
+        const itemsPerPage = 3;
+        const lastOfferIndex = 9;
+        mockItemsPerPage(itemsPerPage);
+        const lastOffer = newOffersByDescUpdatedAt[lastOfferIndex];
+        const { data } = await apolloClient.query({
+          query: GET_MY_OFFERS,
+          variables: {
+            updatedBeforeThan: {
+              dateTime: lastOffer.updatedAt.toISOString(),
+              uuid: lastOffer.uuid
+            }
+          }
+        });
+        expect(data!.getMyOffers.results.map(offer => offer.uuid)).toEqual(
+          newOffersByDescUpdatedAt
+            .slice(lastOfferIndex + 1, lastOfferIndex + 1 + itemsPerPage)
+            .map(offer => offer.uuid)
+        );
+        expect(data!.getMyOffers.shouldFetchMore).toEqual(true);
+      });
     });
   });
 
