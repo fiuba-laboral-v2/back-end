968ca01fd66e8e5378b0341f093572c660df3784
dylan <dylanalvarez1995@gmail.com>
2020-08-27 06:44:45 -0300

    Remove duplicate hardcoded uuid

--- src/seeders/constants/uuids.ts
+++ src/seeders/constants/uuids.ts
@@ -1,46 +1,48 @@
+import generateUuid from "uuid/v4";
+
 export const uuids = {
   companies: {
-    devartis: "7f03fcfa-93a9-476b-881a-b81a7ea9dbd3",
-    mercadoLibre: "70ec7184-bb98-4fd8-93fa-477df4abc060"
+    devartis: generateUuid(),
+    mercadoLibre: generateUuid()
   },
   offers: {
-    java_semi_senior: "005abeec-a28e-41ce-87c1-98bb4f0b16de",
-    java_senior: "7f03fcfb-93a9-476b-881a-b81a7ea9dbd6",
-    java_junior: "238a8b4b-430d-4a4d-b91e-a95a393d1279",
-    ruby_senior: "50af861f-63c5-4c7a-bca1-1e1c1eaacaf3",
-    ruby_junior: "d1c02db9-0926-4c27-a4b9-039c8396fb98",
-    ruby_semi_senior: "c2be9676-9a5b-445e-9999-07b4da818581"
+    java_semi_senior: generateUuid(),
+    java_senior: generateUuid(),
+    java_junior: generateUuid(),
+    ruby_senior: generateUuid(),
+    ruby_junior: generateUuid(),
+    ruby_semi_senior: generateUuid()
   },
   admin: {
-    user: "68755de1-9dcb-44f9-8e08-f8e6ec1f668a"
+    user: generateUuid()
   },
   sebastian: {
-    applicant: "238a8b4b-430d-4a4d-b91e-a95a393d1279",
-    user: "22ae51ae-6eb7-4bd0-8824-9a469f681bb3"
+    applicant: generateUuid(),
+    user: generateUuid()
   },
   aldana: {
-    applicant: "cf6f22c2-b1c9-4bf9-8717-7705e4c651e6",
-    user: "c2be9676-9a5b-445e-9999-07b4da818581"
+    applicant: generateUuid(),
+    user: generateUuid()
   },
   manuel: {
-    user: "84dce80c-8f97-4537-a4c8-2bc8b4b717b4"
+    user: generateUuid()
   },
   mariano: {
-    user: "eaace362-436a-4baa-a2d9-faee82f11888"
+    user: generateUuid()
   },
   capabilities: {
-    node: "9a44d145-ad02-4052-ab17-d9f2f863a4c0",
-    RoR: "c2156144-6aa3-46bf-a48a-12da69d94878",
-    typescript: "7d94ede9-f69c-43e8-ba5f-77bd6c00f99d",
-    python: "1dd69a27-0f6c-4859-be9e-4de5adf22826",
-    go: "6dbc17be-8dea-424e-80f7-20d3b755acdd",
-    c: "4393fdd9-4662-4b6f-8d0d-5de787264264",
-    cPlusPlus: "537bddb6-fe49-4911-abc3-efd763263716",
-    java: "75d96efa-d0db-4372-a856-b623419b4636",
-    postgres: "d3fab5b3-3f2e-4f21-9492-ecf4f48946da",
-    mongo: "6b298513-1a33-4693-ae3e-6e284b42d2f2",
-    cassandra: "dafbf8f9-52cc-4856-917c-d8dcf6121716",
-    sql: "bda5f82a-d839-4af3-ae04-1b669d590a85",
-    noSQL: "fea066b2-3ada-48d5-9437-de51c75298b2"
+    node: generateUuid(),
+    RoR: generateUuid(),
+    typescript: generateUuid(),
+    python: generateUuid(),
+    go: generateUuid(),
+    c: generateUuid(),
+    cPlusPlus: generateUuid(),
+    java: generateUuid(),
+    postgres: generateUuid(),
+    mongo: generateUuid(),
+    cassandra: generateUuid(),
+    sql: generateUuid(),
+    noSQL: generateUuid()
   }
 };
