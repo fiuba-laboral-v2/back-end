0ea9387b07fa65ac2bf71b6f62f58702aae7c606
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-09-21 18:28:28 -0300

    Add canModerateOffer method to the AdminPermissions

--- src/models/Permissions/AdminPermissions/Permissions.ts
+++ src/models/Permissions/AdminPermissions/Permissions.ts
@@ -1,8 +1,10 @@
 import { Offer } from "$models";
 import { IPermission } from "../Interface";
+import { AdminRepository, Secretary } from "$models/Admin";
+import { ApplicantType } from "$models/Applicant";
 
 export class AdminPermissions implements IPermission {
-  public readonly adminUserUuid: string;
+  private readonly adminUserUuid: string;
 
   constructor(adminUserUuid: string) {
     this.adminUserUuid = adminUserUuid;
@@ -10,5 +12,18 @@ export class AdminPermissions implements IPermission {
 
   public canSeeOffer(_: Offer) {
     return Promise.resolve(true);
+  }
+
+  public async canModerateOffer(offer: Offer) {
+    const targetApplicantType = offer.targetApplicantType;
+    if (targetApplicantType === ApplicantType.both) return true;
+
+    const { secretary } = await AdminRepository.findByUserUuid(this.adminUserUuid);
+    return (
+      {
+        [Secretary.graduados]: ApplicantType.graduate,
+        [Secretary.extension]: ApplicantType.student
+      }[secretary] === targetApplicantType
+    );
   }
 }
--- test/models/Permissions/AdminPermissions/Permissions.test.ts
+++ test/models/Permissions/AdminPermissions/Permissions.test.ts
@@ -9,12 +9,14 @@ import { AdminGenerator } from "$generators/Admin";
 
 describe("AdminPermissions", () => {
   let extensionAdmin: Admin;
+  let graduadosAdmin: Admin;
 
   beforeAll(async () => {
     await UserRepository.truncate();
     await CompanyRepository.truncate();
 
     extensionAdmin = await AdminGenerator.extension();
+    graduadosAdmin = await AdminGenerator.graduados();
   });
 
   const createOffer = async () => {
@@ -31,6 +33,50 @@ describe("AdminPermissions", () => {
       expect(await permissions.canSeeOffer(firstOffer)).toBe(true);
       expect(await permissions.canSeeOffer(secondOffer)).toBe(true);
       expect(await permissions.canSeeOffer(thirdOffer)).toBe(true);
+    });
+  });
+
+  describe("canModerateOffer", () => {
+    let companyUuid: string;
+
+    beforeAll(async () => {
+      companyUuid = (await CompanyGenerator.instance.withCompleteData()).uuid;
+    });
+
+    it("returns true if the offer is for students and the admin from extension", async () => {
+      const permissions = new AdminPermissions(extensionAdmin.userUuid);
+      const offer = await OfferGenerator.instance.forStudents({ companyUuid });
+      expect(await permissions.canModerateOffer(offer)).toBe(true);
+    });
+
+    it("returns true if the offer is for both and the admin from extension", async () => {
+      const permissions = new AdminPermissions(extensionAdmin.userUuid);
+      const offer = await OfferGenerator.instance.forStudentsAndGraduates({ companyUuid });
+      expect(await permissions.canModerateOffer(offer)).toBe(true);
+    });
+
+    it("returns true if the offer is for graduates and the admin from graduados", async () => {
+      const permissions = new AdminPermissions(graduadosAdmin.userUuid);
+      const offer = await OfferGenerator.instance.forGraduates({ companyUuid });
+      expect(await permissions.canModerateOffer(offer)).toBe(true);
+    });
+
+    it("returns true if the offer is for both and the admin from graduados", async () => {
+      const permissions = new AdminPermissions(graduadosAdmin.userUuid);
+      const offer = await OfferGenerator.instance.forStudentsAndGraduates({ companyUuid });
+      expect(await permissions.canModerateOffer(offer)).toBe(true);
+    });
+
+    it("returns false if the offer is for graduates and the admin from extension", async () => {
+      const permissions = new AdminPermissions(extensionAdmin.userUuid);
+      const offer = await OfferGenerator.instance.forGraduates({ companyUuid });
+      expect(await permissions.canModerateOffer(offer)).toBe(false);
+    });
+
+    it("returns false if the offer is for students and the admin from graduados", async () => {
+      const permissions = new AdminPermissions(graduadosAdmin.userUuid);
+      const offer = await OfferGenerator.instance.forStudents({ companyUuid });
+      expect(await permissions.canModerateOffer(offer)).toBe(false);
     });
   });
 });
