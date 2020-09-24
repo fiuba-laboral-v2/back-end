50841b09ac9a0b7b35f4cb06ee5c77ba1eb942a3
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-09-24 20:38:28 -0300

    Reuse where clause builders in the WhereClauseBuilder model

--- src/models/AdminTask/WhereClauseBuilder/WhereClauseBuilder.ts
+++ src/models/AdminTask/WhereClauseBuilder/WhereClauseBuilder.ts
@@ -1,59 +1,22 @@
 import { ApprovalStatus } from "$models/ApprovalStatus";
 import { Secretary } from "$models/Admin";
 import { IPaginatedInput } from "$graphql/Pagination/Types/GraphQLPaginatedInput";
-import {
-  AdminTaskType,
-  SeparateApprovalAdminTaskTypes,
-  SharedApprovalAdminTaskTypes
-} from "$models/AdminTask/Model";
-import { TargetWhereClauseBuilder } from "./TargetWhereClauseBuilder";
-import intersection from "lodash/intersection";
+import { AdminTaskType } from "$models/AdminTask/Model";
+import { StatusWhereClause } from "./Status";
+import { OfferTargetWhereClause } from "./OfferTarget";
+import { ApplicantTypeWhereClause } from "./ApplicantType";
+import { UpdatedAtWhereClause } from "./UpdatedAt";
 
 export const WhereClauseBuilder = {
   build: ({ statuses, secretary, adminTaskTypes, updatedBeforeThan }: IWhereClauseBuilder) =>
     [
-      WhereClauseBuilder.getStatusWhereClause(statuses, secretary, adminTaskTypes),
-      TargetWhereClauseBuilder.build({ secretary, adminTaskTypes }),
-      WhereClauseBuilder.getUpdatedAtWhereClause(updatedBeforeThan)
+      StatusWhereClause.build({ statuses, secretary, adminTaskTypes }),
+      OfferTargetWhereClause.build({ secretary, adminTaskTypes }),
+      ApplicantTypeWhereClause.build({ secretary, adminTaskTypes }),
+      UpdatedAtWhereClause.build(updatedBeforeThan)
     ]
       .filter(clause => clause)
-      .join(" AND "),
-  getUpdatedAtWhereClause: (updatedBeforeThan?: IPaginatedInput) => {
-    if (!updatedBeforeThan) return;
-    const updatedAtString = updatedBeforeThan.dateTime.toISOString();
-    return `
-      (
-        ("AdminTask"."updatedAt" < '${updatedAtString}')
-        OR (
-          "AdminTask"."updatedAt" = '${updatedAtString}'
-          AND "AdminTask"."uuid" < '${updatedBeforeThan.uuid}'
-        )
-      )
-    `;
-  },
-  getStatusWhereClause: (
-    statuses: ApprovalStatus[],
-    secretary: Secretary,
-    adminTaskTypes: AdminTaskType[]
-  ) => {
-    const conditions: string[] = [];
-    const columns: string[] = [];
-    if (intersection(adminTaskTypes, SharedApprovalAdminTaskTypes).length > 0) {
-      columns.push("approvalStatus");
-    }
-    if (intersection(adminTaskTypes, SeparateApprovalAdminTaskTypes).length > 0) {
-      columns.push(
-        secretary === Secretary.graduados ? "graduadosApprovalStatus" : "extensionApprovalStatus"
-      );
-    }
-
-    for (const column of columns) {
-      for (const status of statuses) {
-        conditions.push(`"AdminTask"."${column}" = '${status}'`);
-      }
-    }
-    return `(${conditions.join(" OR ")})`;
-  }
+      .join(" AND ")
 };
 
 interface IWhereClauseBuilder {
