fb92fdf33f82a867d6a060cc514778725ad25c7c
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-10-02 20:09:48 -0300

    Declare each environment variable key

--- src/config/Environment.ts
+++ src/config/Environment.ts
@@ -1,19 +1,29 @@
+const variablesKeys = {
+  NODE_ENV: "NODE_ENV",
+  DATABASE_URL: "DATABASE_URL",
+  JWT_SECRET: "JWT_SECRET",
+  FIUBA_USERS_API_URL: "FIUBA_USERS_API_URL",
+  EMAIL_API_APPLICATION_ID: "EMAIL_API_APPLICATION_ID",
+  EMAIL_API_PASSWORD: "EMAIL_API_PASSWORD",
+  EMAIL_API_URL: "EMAIL_API_URL"
+};
+
 export const Environment = {
   PRODUCTION: "production",
   STAGING: "staging",
   DEVELOPMENT: "development",
   TEST_TRAVIS: "test_travis",
   TEST: "test",
-  NODE_ENV: process.env.NODE_ENV || "development",
-  databaseURL: () => process.env.DATABASE_URL,
-  JWTSecret: () => process.env.JWT_SECRET,
+  NODE_ENV: process.env[variablesKeys.NODE_ENV] || "development",
+  databaseURL: () => process.env[variablesKeys.DATABASE_URL],
+  JWTSecret: () => process.env[variablesKeys.JWT_SECRET],
   FiubaUsersApi: {
-    url: () => process.env.FIUBA_USERS_API_URL as string
+    url: () => process.env[variablesKeys.FIUBA_USERS_API_URL] as string
   },
   emailApi: {
-    applicationID: () => process.env.EMAIL_API_APPLICATION_ID,
-    password: () => process.env.EMAIL_API_PASSWORD,
-    url: () => process.env.EMAIL_API_URL as string
+    applicationID: () => process.env[variablesKeys.EMAIL_API_APPLICATION_ID],
+    password: () => process.env[variablesKeys.EMAIL_API_PASSWORD],
+    url: () => process.env[variablesKeys.EMAIL_API_URL] as string
   },
   isLocal() {
     return [this.DEVELOPMENT, this.TEST, this.TEST_TRAVIS].includes(this.NODE_ENV);
@@ -22,12 +32,12 @@ export const Environment = {
     if (this.isLocal()) return;
 
     const mandatoryVariables = [
-      { name: "Database URL", value: this.databaseURL() },
-      { name: "JWT secret", value: this.JWTSecret() },
-      { name: "Email API application ID", value: this.emailApi.applicationID() },
-      { name: "Email API password", value: this.emailApi.password() },
-      { name: "Email API url", value: this.emailApi.url() },
-      { name: "Fiuba Users API url", value: this.FiubaUsersApi.url() }
+      { name: variablesKeys.DATABASE_URL, value: this.databaseURL() },
+      { name: variablesKeys.JWT_SECRET, value: this.JWTSecret() },
+      { name: variablesKeys.EMAIL_API_APPLICATION_ID, value: this.emailApi.applicationID() },
+      { name: variablesKeys.EMAIL_API_PASSWORD, value: this.emailApi.password() },
+      { name: variablesKeys.EMAIL_API_URL, value: this.emailApi.url() },
+      { name: variablesKeys.FIUBA_USERS_API_URL, value: this.FiubaUsersApi.url() }
     ];
 
     mandatoryVariables.map(({ name, value }) => {
