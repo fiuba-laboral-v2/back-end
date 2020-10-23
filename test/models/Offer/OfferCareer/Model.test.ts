25d3fbcb46df1434cfad5d6b7c2189eee60caccd
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-10-22 23:46:52 -0300

    Simplify OfferCareer test

--- test/models/Offer/OfferCareer/Model.test.ts
+++ test/models/Offer/OfferCareer/Model.test.ts
@@ -1,36 +1,33 @@
 import { ValidationError } from "sequelize";
 import { OfferCareer } from "$models";
+import generateUuid from "uuid/v4";
 
 describe("OfferCareer", () => {
-  describe("Valid create", () => {
-    it("should create a valid offer career", async () => {
-      const offerCareer = new OfferCareer({
-        offerUuid: "4c925fdc-8fd4-47ed-9a24-fa81ed5cc9da",
-        careerCode: "00608d97-0603-4127-825b-d8e40f6e8ccd"
-      });
-      await expect(offerCareer.validate()).resolves.not.toThrow();
-    });
+  const attributes = {
+    offerUuid: generateUuid(),
+    careerCode: generateUuid()
+  };
+
+  const expectToThrowErrorOnMissingAttribute = async (attribute: string) => {
+    delete attributes[attribute];
+    const section = new OfferCareer(attributes);
+    await expect(section.validate()).rejects.toThrowErrorWithMessage(
+      ValidationError,
+      `notNull Violation: OfferCareer.${attribute} cannot be null`
+    );
+  };
+
+  it("creates a valid offer career", async () => {
+    const offerCareer = new OfferCareer(attributes);
+    await expect(offerCareer.validate()).resolves.not.toThrow();
+    expect(offerCareer).toBeObjectContaining(attributes);
   });
 
-  describe("Errors", () => {
-    it("should throw an error if no careerCode is provided", async () => {
-      const offerCareer = new OfferCareer({
-        offerUuid: "4c925fdc-8fd4-47ed-9a24-fa81ed5cc9da"
-      });
-      await expect(offerCareer.validate()).rejects.toThrowErrorWithMessage(
-        ValidationError,
-        "notNull Violation: OfferCareer.careerCode cannot be null"
-      );
-    });
+  it("throws an error if no careerCode is provided", async () => {
+    await expectToThrowErrorOnMissingAttribute("careerCode");
+  });
 
-    it("should throw an error if no offerUuid is provided", async () => {
-      const offerCareer = new OfferCareer({
-        careerCode: "4c925fdc-8fd4-47ed-9a24-fa81ed5cc9da"
-      });
-      await expect(offerCareer.validate()).rejects.toThrowErrorWithMessage(
-        ValidationError,
-        "notNull Violation: OfferCareer.offerUuid cannot be null"
-      );
-    });
+  it("throws an error if no offerUuid is provided", async () => {
+    await expectToThrowErrorOnMissingAttribute("offerUuid");
   });
 });
