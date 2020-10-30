825ff4b6ef65883f6216d5ab3f6639667036be3c
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-10-30 00:22:05 -0300

    Fix toEqualGraphQLErrorType tye declaration

--- test/types/index.d.ts
+++ test/types/index.d.ts
@@ -15,7 +15,7 @@ declare global {
       toBeSortedBy(options: IOptions): R;
       toEqualIgnoringSpacing(expected: string): R;
       toBeObjectContaining(expected: object): R;
-      toEqualGraphQLErrorType(errorType: Constructable | string): R;
+      toEqualGraphQLErrorType(errorType: string): R;
     }
   }
 }
