7b66494ce198c31ae5bb0acba7ee545746d50b62
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-11-08 02:19:10 -0300

    Throw MissingNotificationTypeError on the GraphQLNotification union type

--- src/graphql/Notification/Types/GraphQLNotification.ts
+++ src/graphql/Notification/Types/GraphQLNotification.ts
@@ -1,12 +1,13 @@
 import { GraphQLUnionType } from "graphql";
 import { GraphQLJobApplicationNotification } from "./GraphQLJobApplicationNotification";
 import { Notification } from "$models";
+import { MissingNotificationTypeError } from "$models/Notification";
 
 export const GraphQLNotification = new GraphQLUnionType({
   name: "Notification",
   types: [GraphQLJobApplicationNotification],
   resolveType(value: Notification) {
     if (value.jobApplicationUuid) return GraphQLJobApplicationNotification;
-    throw new Error("Value is not of Notification type");
+    throw new MissingNotificationTypeError();
   }
 });
