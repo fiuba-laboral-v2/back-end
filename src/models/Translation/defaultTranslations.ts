c1d9336120c3f037b3245efb74eb85c58751346a
llmanuel <llauromanuel@gmail.com>
2020-11-15 12:37:31 -0300

    attend comments

--- src/models/Translation/defaultTranslations.ts
+++ src/models/Translation/defaultTranslations.ts
@@ -27,14 +27,14 @@ export const defaultTranslations = {
     {
       key: "message",
       value:
-        "La oferta será revisada por personal de FIUBA. Una vez aprobada, permanecerá publicada por %{alumnosDuration} días para alumnos y/o %{graduadosDuration} días para graduados."
+        "La oferta será revisada por personal de FIUBA. Una vez aprobada, permanecerá publicada por <%= studentsOfferDuration %> días para alumnos y/o <%= graduatesOfferDuration %> días para graduados, a menos que la publicación sea removida."
     }
   ],
   offerEditionInfoMessage: [
     {
       key: "message",
       value:
-        "Al editar una oferta, la misma volverá a ser revisada por personal de FIUBA. Una vez aprobada, permanecerá publicada por %{alumnosDuration} días para alumnos y/o %{graduadosDuration} días para graduados."
+        "Al editar una oferta, la misma volverá a ser revisada por personal de FIUBA. Una vez aprobada, permanecerá publicada por <%= studentsOfferDuration %> días para alumnos y/o <%= graduatesOfferDuration %> días para graduados, a menos que la publicación sea removida."
     }
   ],
   jobApplicationEmptyOfferList: [
