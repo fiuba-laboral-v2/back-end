cc48fe92d8cc09fcf559878806557719967adb6d
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-11-21 02:49:09 -0300

    Delete MultipleTypeNotificationError

--- src/models/Notification/Errors/MultipleTypeNotificationError.ts
+++
@@ -1,9 +0,0 @@
-export class MultipleTypeNotificationError extends Error {
-  public static buildMessage() {
-    return "Notification can only be of one type";
-  }
-
-  constructor() {
-    super(MultipleTypeNotificationError.buildMessage());
-  }
-}
