a639f5dcd399b6ea9ac299e2e34dc246ef9084f8
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-11-23 19:31:23 -0300

    Refactor CompanyNewJobApplicationEmailSender by moving code to internal functions

--- src/services/EmailSender/CompanyNewJobApplicationEmailSender/Service.ts
+++ src/services/EmailSender/CompanyNewJobApplicationEmailSender/Service.ts
@@ -10,40 +10,49 @@ import { template } from "lodash";
 import { ApplicantRepository } from "$models/Applicant";
 import { AdminRepository } from "$models/Admin";
 
+const getReceiverEmails = async (companyUuid: string) => {
+  const companyUsers = await CompanyUserRepository.findByCompanyUuid(companyUuid);
+  const receivers = await UserRepository.findByUuids(companyUsers.map(({ userUuid }) => userUuid));
+  return receivers.map(({ email }) => email);
+};
+
+const getSender = async (adminUserUuid: string) => {
+  const sender = await UserRepository.findByUuid(adminUserUuid);
+  return {
+    email: sender.email,
+    name: `${sender.name} ${sender.surname}`
+  };
+};
+
+const getSignature = async (adminUserUuid: string): Promise<string> => {
+  const admin = await AdminRepository.findByUserUuid(adminUserUuid);
+  const signatures = TranslationRepository.translate("emailSignature");
+  return signatures.find(({ key }) => key === admin.secretary).value;
+};
+
 export const CompanyNewJobApplicationEmailSender = {
   send: async (notification: CompanyNewJobApplicationNotification) => {
-    const companyUuid = notification.notifiedCompanyUuid;
-    const companyUsers = await CompanyUserRepository.findByCompanyUuid(companyUuid);
-    const userUuids = companyUsers.map(({ userUuid }) => userUuid);
-    const users = await UserRepository.findByUuids(userUuids);
-    const receiverEmails = users.map(({ email }) => email);
-    const sender = await UserRepository.findByUuid(notification.moderatorUuid);
     const { offerUuid, applicantUuid } = await JobApplicationRepository.findByUuid(
       notification.jobApplicationUuid
     );
     const offer = await OfferRepository.findByUuid(offerUuid);
-    const admin = await AdminRepository.findByUserUuid(notification.moderatorUuid);
-    const signatures = TranslationRepository.translate("emailSignature");
-    const signature = signatures.find(({ key }) => key === admin.secretary).value;
     const [subject, body] = TranslationRepository.translate(
       "companyNewJobApplicationNotificationEmail"
     );
     const applicant = await ApplicantRepository.findByUuid(applicantUuid);
     const applicantUser = await UserRepository.findByUuid(applicant.userUuid);
     const { baseUrl, subDomain, endpoints } = FrontendConfig;
+
     return EmailService.send({
-      receiverEmails,
-      sender: {
-        email: sender.email,
-        name: `${sender.name} ${sender.surname}`
-      },
+      receiverEmails: await getReceiverEmails(notification.notifiedCompanyUuid),
+      sender: await getSender(notification.moderatorUuid),
       subject: subject.value,
       body: template(body.value)({
         offerTitle: offer.title,
         offerLink: `${baseUrl}/${subDomain}/${endpoints.company.offer(offerUuid)}`,
         applicantName: `${applicantUser.name} ${applicantUser.surname}`,
         ApplicantLink: `${baseUrl}/${subDomain}/${endpoints.company.applicant(applicantUuid)}`,
-        signature
+        signature: await getSignature(notification.moderatorUuid)
       })
     });
   }
