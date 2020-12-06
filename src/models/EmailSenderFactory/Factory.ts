e4b33b83cfba5fa19baa95cae76d9c1fd9905eb1
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-06 17:52:57 -0300

    Add ApprovedOfferCompanyNotificationEmailSender to the EmailSenderFactory

--- src/models/EmailSenderFactory/Factory.ts
+++ src/models/EmailSenderFactory/Factory.ts
@@ -1,15 +1,26 @@
 import { TNotification } from "$models/Notification/Model";
-import { NewJobApplicationCompanyNotification } from "$models/CompanyNotification";
-import { NewJobApplicationCompanyNotificationEmailSender } from "$services/EmailSender";
+import {
+  NewJobApplicationCompanyNotification,
+  ApprovedOfferCompanyNotification
+} from "$models/CompanyNotification";
+import {
+  NewJobApplicationCompanyNotificationEmailSender,
+  ApprovedOfferCompanyNotificationEmailSender
+} from "$services/EmailSender";
 
 export const EmailSenderFactory = {
-  create: (notification: TNotification) => {
+  create: (notification: TNotification): IEmailSenderFactory => {
     const className = notification.constructor.name;
     const emailSender = {
-      [NewJobApplicationCompanyNotification.name]: NewJobApplicationCompanyNotificationEmailSender
+      [NewJobApplicationCompanyNotification.name]: NewJobApplicationCompanyNotificationEmailSender,
+      [ApprovedOfferCompanyNotification.name]: ApprovedOfferCompanyNotificationEmailSender
     }[className];
     if (!emailSender) throw new Error(`no emailSender found for ${className}`);
 
     return emailSender;
   }
 };
+
+interface IEmailSenderFactory {
+  send: (notifications: TNotification) => Promise<void>;
+}
