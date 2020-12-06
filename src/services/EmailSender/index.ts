36bd145b3e692f1bcf52216941090e1e6380047c
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-06 17:03:15 -0300

    Add ApprovedOfferCompanyNotificationEmailSender

--- src/services/EmailSender/index.ts
+++ src/services/EmailSender/index.ts
@@ -1,1 +1,2 @@
 export * from "./NewJobApplicationCompanyNotificationEmailSender";
+export * from "./ApprovedOfferCompanyNotificationEmailSender";
--- test/services/EmailSender/ApprovedOfferCompanyNotificationEmailSender/Service.test.ts
+++ test/services/EmailSender/ApprovedOfferCompanyNotificationEmailSender/Service.test.ts
@@ -1,19 +1,18 @@
 import { UserRepository } from "$models/User";
 import { CompanyRepository } from "$models/Company";
 import { CareerRepository } from "$models/Career";
-import { JobApplicationRepository } from "$models/JobApplication";
-import { AdminRepository, Secretary } from "$models/Admin";
+import { OfferRepository } from "$models/Offer";
 import { SecretarySettingsRepository } from "$models/SecretarySettings";
-
-import { NewJobApplicationCompanyNotificationEmailSender } from "$services/EmailSender";
 import { EmailService } from "$services/Email";
+import { ApprovedOfferCompanyNotificationEmailSender } from "$services/EmailSender";
 
 import { CompanyGenerator } from "$generators/Company";
 import { CompanyNotificationGenerator } from "$generators/CompanyNotification";
 import { AdminGenerator } from "$generators/Admin";
 import { SecretarySettingsGenerator } from "$generators/SecretarySettings";
+import { AdminRepository, Secretary } from "$models/Admin";
 
-describe("NewJobApplicationCompanyNotificationEmailSender", () => {
+describe("ApprovedOfferCompanyNotificationEmailSender", () => {
   beforeAll(async () => {
     await UserRepository.truncate();
     await CompanyRepository.truncate();
@@ -23,22 +22,21 @@ describe("NewJobApplicationCompanyNotificationEmailSender", () => {
     await SecretarySettingsGenerator.createDefaultSettings();
   });
 
-  it("sends an email to alñ company users that a new application has arrived", async () => {
+  it("sends an email to all company users that an offer has been approved", async () => {
     const companyAttributes = CompanyGenerator.data.completeData();
     const company = await CompanyRepository.create(companyAttributes);
     const adminAttributes = AdminGenerator.data(Secretary.graduados);
     const admin = await AdminRepository.create(adminAttributes);
-    const notification = await CompanyNotificationGenerator.instance.newJobApplication({
+    const notification = await CompanyNotificationGenerator.instance.approvedOffer({
       company,
       admin
     });
+    const offer = await OfferRepository.findByUuid(notification.offerUuid);
+
     const emailSendMock = jest.fn();
     jest.spyOn(EmailService, "send").mockImplementation(emailSendMock);
-    await NewJobApplicationCompanyNotificationEmailSender.send(notification);
+    await ApprovedOfferCompanyNotificationEmailSender.send(notification);
 
-    const { offerUuid, applicantUuid } = await JobApplicationRepository.findByUuid(
-      notification.jobApplicationUuid
-    );
     expect(emailSendMock.mock.calls).toEqual([
       [
         {
@@ -47,11 +45,9 @@ describe("NewJobApplicationCompanyNotificationEmailSender", () => {
             name: `${adminAttributes.user.name} ${adminAttributes.user.surname}`,
             email: adminAttributes.user.email
           },
-          subject: "Nueva postulación a tu oferta laboral",
+          subject: "Oferta laboral aprobada",
           body: expect.stringContaining(
-            `Nueva postulación a tu oferta laboral: title1 (baseUrl/subDomain/empresa/ofertas/${offerUuid})` +
-              "\n" +
-              `Postulante: applicantName applicantSurname (baseUrl/subDomain/empresa/postulantes/${applicantUuid}).` +
+            `Tu oferta laboral ha sido aprobada: ${offer.title} (baseUrl/subDomain/empresa/ofertas/${offer.uuid})` +
               "\n\n" +
               `Bolsa de Trabajo FIUBA`
           )
