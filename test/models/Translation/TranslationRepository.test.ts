f4778ce5023758adca36e3e273d0c7fe996d9bc1
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-07 21:35:28 -0300

    Add findSignatureByAdmin method to the TranslationRepository

--- test/models/Translation/TranslationRepository.test.ts
+++ test/models/Translation/TranslationRepository.test.ts
@@ -1,13 +1,32 @@
+import { UserRepository } from "$models/User";
 import { TranslationRepository } from "$models/Translation";
 import { MissingTranslationError } from "$models/Translation/Errors";
 
+import { AdminGenerator } from "$generators/Admin";
+
 describe("TranslationRepository", () => {
+  beforeAll(async () => {
+    await UserRepository.truncate();
+  });
+
   it("returns the translations that belong to the given translationGroup", () => {
     const translations = TranslationRepository.translate("applicantProfileTitle");
     expect(translations).toEqual({
       title: "Mi Perfil",
       subtitle: "Así lo va a ver una empresa"
     });
+  });
+
+  it("returns the signature from an extension admin", async () => {
+    const extensionAdmin = await AdminGenerator.extension();
+    const signature = await TranslationRepository.findSignatureByAdmin(extensionAdmin.userUuid);
+    expect(signature).toEqual("Bolsa de Trabajo FIUBA");
+  });
+
+  it("returns the signature from a graduados admin", async () => {
+    const graduadosAdmin = await AdminGenerator.graduados();
+    const signature = await TranslationRepository.findSignatureByAdmin(graduadosAdmin.userUuid);
+    expect(signature).toEqual("Bolsa de Trabajo FIUBA");
   });
 
   it("throws an error if translationGroup does not exist", () => {
