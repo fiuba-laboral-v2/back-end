1e9102b995eb68f7f6767a6fd006e82570d0cd24
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-10 15:10:00 -0300

    Test RejectedJobApplicationApplicantNotification persistence in the ApplicantNotificationRepository

--- src/migrations/20201207183940-create-applicant-notification-enum-type.ts
+++ src/migrations/20201207183940-create-applicant-notification-enum-type.ts
@@ -3,7 +3,7 @@ import { QueryInterface } from "sequelize";
 export = {
   up: (queryInterface: QueryInterface) =>
     queryInterface.sequelize.query(
-      "CREATE TYPE applicant_notification_type AS ENUM ('approvedJobApplication');"
+      "CREATE TYPE applicant_notification_type AS ENUM ('approvedJobApplication', 'rejectedJobApplication');"
     ),
   down: (queryInterface: QueryInterface) =>
     queryInterface.sequelize.query("DROP TYPE applicant_notification_type;")
