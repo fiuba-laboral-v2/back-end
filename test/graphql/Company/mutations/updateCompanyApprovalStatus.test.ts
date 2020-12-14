5a18c259caf8d5126db59ef2361414077a4cc47f
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-14 00:40:28 -0300

    Use UUID model in the updateCompanyApprovalStatus mutation test

--- test/graphql/Company/mutations/updateCompanyApprovalStatus.test.ts
+++ test/graphql/Company/mutations/updateCompanyApprovalStatus.test.ts
@@ -1,15 +1,19 @@
 import { gql } from "apollo-server";
 import { ApolloServerTestClient as TestClient } from "apollo-server-testing";
 import { client } from "../../ApolloTestClient";
-import { TestClientGenerator } from "$generators/TestClient";
-import { CompanyGenerator } from "$generators/Company";
-import { CompanyRepository } from "$models/Company";
+
+import { UUID } from "$models/UUID";
 import { Company } from "$models";
-import { CompanyApprovalEventRepository } from "$models/Company/CompanyApprovalEvent";
-import { UserRepository } from "$models/User";
 import { ApprovalStatus } from "$models/ApprovalStatus";
 import { AuthenticationError, UnauthorizedError } from "$graphql/Errors";
 import { CompanyNotUpdatedError } from "$models/Company/Errors";
+
+import { CompanyApprovalEventRepository } from "$models/Company/CompanyApprovalEvent";
+import { UserRepository } from "$models/User";
+import { CompanyRepository } from "$models/Company";
+
+import { TestClientGenerator } from "$generators/TestClient";
+import { CompanyGenerator } from "$generators/Company";
 
 const UPDATE_COMPANY_APPROVAL_STATUS = gql`
   mutation($uuid: ID!, $approvalStatus: ApprovalStatus!) {
@@ -67,51 +71,35 @@ describe("updateCompanyApprovalStatus", () => {
 
   it("throws an error if no user is logged in", async () => {
     const apolloClient = client.loggedOut();
-    const dataToUpdate = {
-      uuid: company.uuid,
-      approvalStatus: ApprovalStatus.approved
-    };
+    const dataToUpdate = { uuid: company.uuid, approvalStatus: ApprovalStatus.approved };
     const { errors } = await performMutation(apolloClient, dataToUpdate);
     expect(errors).toEqualGraphQLErrorType(AuthenticationError.name);
   });
 
   it("throws an error if the current user is an applicant", async () => {
     const { apolloClient } = await TestClientGenerator.applicant();
-    const dataToUpdate = {
-      uuid: company.uuid,
-      approvalStatus: ApprovalStatus.approved
-    };
+    const dataToUpdate = { uuid: company.uuid, approvalStatus: ApprovalStatus.approved };
     const { errors } = await performMutation(apolloClient, dataToUpdate);
     expect(errors).toEqualGraphQLErrorType(UnauthorizedError.name);
   });
 
   it("throws an error if the current user is from a company", async () => {
     const { apolloClient } = await TestClientGenerator.company();
-    const dataToUpdate = {
-      uuid: company.uuid,
-      approvalStatus: ApprovalStatus.approved
-    };
+    const dataToUpdate = { uuid: company.uuid, approvalStatus: ApprovalStatus.approved };
     const { errors } = await performMutation(apolloClient, dataToUpdate);
     expect(errors).toEqualGraphQLErrorType(UnauthorizedError.name);
   });
 
   it("throws an error if the company does not exists", async () => {
-    const nonExistentCompanyUuid = "4c925fdc-8fd4-47ed-9a24-fa81ed5cc9da";
     const { apolloClient } = await TestClientGenerator.admin();
-    const dataToUpdate = {
-      uuid: nonExistentCompanyUuid,
-      approvalStatus: ApprovalStatus.approved
-    };
+    const dataToUpdate = { uuid: UUID.generate(), approvalStatus: ApprovalStatus.approved };
     const { errors } = await performMutation(apolloClient, dataToUpdate);
     expect(errors).toEqualGraphQLErrorType(CompanyNotUpdatedError.name);
   });
 
   it("throws an error if the approvalStatus is invalid", async () => {
     const { apolloClient } = await TestClientGenerator.admin();
-    const dataToUpdate = {
-      uuid: company.uuid,
-      approvalStatus: "invalidApprovalStatus"
-    };
+    const dataToUpdate = { uuid: company.uuid, approvalStatus: "invalidApprovalStatus" };
     const { errors } = await performMutation(apolloClient, dataToUpdate);
     expect(errors).not.toBeUndefined();
   });
