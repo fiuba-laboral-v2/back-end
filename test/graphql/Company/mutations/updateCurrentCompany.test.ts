6f198c537f2603f2bf980b480c51c0f2b9c4ea72
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-15 21:04:07 -0300

    Test permissions in the updateCurrentCompany mutation

--- test/graphql/Company/mutations/updateCurrentCompany.test.ts
+++ test/graphql/Company/mutations/updateCurrentCompany.test.ts
@@ -1,12 +1,17 @@
 import { gql } from "apollo-server";
+import { client } from "../../ApolloTestClient";
+import { ApolloServerTestClient as TestClient } from "apollo-server-testing/dist/createTestClient";
 
-import { TestClientGenerator } from "$generators/TestClient";
+import { AuthenticationError, UnauthorizedError } from "$graphql/Errors";
+import { ApprovalStatus } from "$models/ApprovalStatus";
+import { Secretary } from "$models/Admin";
+import { Admin } from "$models";
 
 import { CompanyRepository } from "$models/Company";
 import { UserRepository } from "$models/User";
 
-import { client } from "../../ApolloTestClient";
-import { AuthenticationError, UnauthorizedError } from "$graphql/Errors";
+import { TestClientGenerator } from "$generators/TestClient";
+import { AdminGenerator } from "$generators/Admin";
 
 const UPDATE_CURRENT_COMPANY = gql`
   mutation(
@@ -40,52 +45,118 @@ const UPDATE_CURRENT_COMPANY = gql`
 `;
 
 describe("updateCurrentCompany", () => {
+  let admin: Admin;
+
   beforeAll(async () => {
     await CompanyRepository.truncate();
     await UserRepository.truncate();
+
+    admin = await AdminGenerator.graduados();
   });
 
-  describe("When the update succeeds", () => {
-    it("update all company attributes", async () => {
-      const { apolloClient } = await TestClientGenerator.company();
-      const dataToUpdate = {
-        companyName: "Devartis SA",
-        slogan: "new slogan",
-        description: "new description",
-        logo: "",
-        website: "http://www.new-site.com",
-        email: "old@devartis.com"
-      };
-      const { data, errors } = await apolloClient.mutate({
-        mutation: UPDATE_CURRENT_COMPANY,
-        variables: dataToUpdate
-      });
-      expect(errors).toBeUndefined();
-      expect(data!.updateCurrentCompany).toEqual(dataToUpdate);
+  const performQuery = (apolloClient: TestClient, variables?: object) =>
+    apolloClient.mutate({
+      mutation: UPDATE_CURRENT_COMPANY,
+      variables: variables || {}
     });
+
+  const createCompanyTestClient = (approvalStatus: ApprovalStatus) =>
+    TestClientGenerator.company({ status: { approvalStatus, admin } });
+
+  const createApplicantTestClient = (approvalStatus: ApprovalStatus) =>
+    TestClientGenerator.applicant({ status: { approvalStatus, admin } });
+
+  it("update all company attributes", async () => {
+    const { apolloClient } = await createCompanyTestClient(ApprovalStatus.pending);
+    const dataToUpdate = {
+      companyName: "Devartis SA",
+      slogan: "new slogan",
+      description: "new description",
+      logo: "",
+      website: "http://www.new-site.com",
+      email: "old@devartis.com"
+    };
+    const { data, errors } = await apolloClient.mutate({
+      mutation: UPDATE_CURRENT_COMPANY,
+      variables: dataToUpdate
+    });
+    expect(errors).toBeUndefined();
+    expect(data!.updateCurrentCompany).toEqual(dataToUpdate);
   });
 
-  describe("when the update errors", () => {
-    it("throws an error if there is no current user", async () => {
-      const apolloClient = client.loggedOut();
-      const dataToUpdate = { companyName: "new company name" };
-      const { errors } = await apolloClient.mutate({
-        mutation: UPDATE_CURRENT_COMPANY,
-        variables: dataToUpdate
-      });
+  it("allows a pending company to update", async () => {
+    const { apolloClient } = await createCompanyTestClient(ApprovalStatus.pending);
+    const { errors } = await performQuery(apolloClient, { companyName: "Devartis SA" });
+    expect(errors).toBeUndefined();
+  });
 
-      expect(errors).toEqualGraphQLErrorType(AuthenticationError.name);
+  it("allows a rejected company to update", async () => {
+    const { apolloClient } = await createCompanyTestClient(ApprovalStatus.rejected);
+    const { errors } = await performQuery(apolloClient, { companyName: "Devartis SA" });
+    expect(errors).toBeUndefined();
+  });
+
+  it("allows an approved company to update", async () => {
+    const { apolloClient } = await createCompanyTestClient(ApprovalStatus.approved);
+    const { errors } = await performQuery(apolloClient, { companyName: "Devartis SA" });
+    expect(errors).toBeUndefined();
+  });
+
+  it("throws an error if there is no current user", async () => {
+    const apolloClient = client.loggedOut();
+    const dataToUpdate = { companyName: "new company name" };
+    const { errors } = await apolloClient.mutate({
+      mutation: UPDATE_CURRENT_COMPANY,
+      variables: dataToUpdate
     });
 
-    it("throws an error if current user is not a company user", async () => {
-      const { apolloClient } = await TestClientGenerator.user();
-      const dataToUpdate = { companyName: "new company name" };
-      const { errors } = await apolloClient.mutate({
-        mutation: UPDATE_CURRENT_COMPANY,
-        variables: dataToUpdate
-      });
+    expect(errors).toEqualGraphQLErrorType(AuthenticationError.name);
+  });
 
-      expect(errors).toEqualGraphQLErrorType(UnauthorizedError.name);
+  it("throws an error if current user is not a company user", async () => {
+    const { apolloClient } = await TestClientGenerator.user();
+    const { errors } = await apolloClient.mutate({
+      mutation: UPDATE_CURRENT_COMPANY,
+      variables: {}
     });
+    expect(errors).toEqualGraphQLErrorType(UnauthorizedError.name);
+  });
+
+  it("returns an error if there is no current user", async () => {
+    const apolloClient = client.loggedOut();
+    const { errors } = await performQuery(apolloClient);
+    expect(errors).toEqualGraphQLErrorType(AuthenticationError.name);
+  });
+
+  it("returns an error if the current user is a approved applicant", async () => {
+    const { apolloClient } = await createApplicantTestClient(ApprovalStatus.approved);
+    const { errors } = await performQuery(apolloClient);
+    expect(errors).toEqualGraphQLErrorType(UnauthorizedError.name);
+  });
+
+  it("returns an error if the current user is a rejected applicant", async () => {
+    const { apolloClient } = await createApplicantTestClient(ApprovalStatus.rejected);
+    const { errors } = await performQuery(apolloClient);
+    expect(errors).toEqualGraphQLErrorType(UnauthorizedError.name);
+  });
+
+  it("returns an error if the current user is a pending applicant", async () => {
+    const { apolloClient } = await createApplicantTestClient(ApprovalStatus.pending);
+    const { errors } = await performQuery(apolloClient);
+    expect(errors).toEqualGraphQLErrorType(UnauthorizedError.name);
+  });
+
+  it("returns an error if the current user is an extension admin", async () => {
+    const secretary = Secretary.extension;
+    const { apolloClient } = await TestClientGenerator.admin({ secretary });
+    const { errors } = await performQuery(apolloClient);
+    expect(errors).toEqualGraphQLErrorType(UnauthorizedError.name);
+  });
+
+  it("returns an error if the current user is a graduados admin", async () => {
+    const secretary = Secretary.graduados;
+    const { apolloClient } = await TestClientGenerator.admin({ secretary });
+    const { errors } = await performQuery(apolloClient);
+    expect(errors).toEqualGraphQLErrorType(UnauthorizedError.name);
   });
 });
