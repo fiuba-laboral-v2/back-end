fdd2ab1cba9a9d1f2deb0207bc3fa9f3a05616c3
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-21 18:19:54 -0300

    Add role to CompanyUser

--- test/models/CompanyUser/Model.test.ts
+++ test/models/CompanyUser/Model.test.ts
@@ -1,11 +1,13 @@
 import { CompanyUser } from "$models";
 import { ValidationError } from "sequelize";
 import { UUID } from "$models/UUID";
+import { omit } from "lodash";
 
 describe("CompanyUser", () => {
   const mandatoryAttributes = {
     companyUuid: UUID.generate(),
-    userUuid: UUID.generate()
+    userUuid: UUID.generate(),
+    role: "RR.HH"
   };
 
   it("creates a valid companyUser", async () => {
@@ -24,16 +26,17 @@ describe("CompanyUser", () => {
     expect(companyUser.updatedAt).toBeUndefined();
   });
 
-  it("throws an error if no companyUuid and no userUuid are provided", async () => {
+  it("throws an error if no companyUuid, no userUuid and no role are provided", async () => {
     const companyUser = new CompanyUser();
     await expect(companyUser.validate()).rejects.toThrowErrorWithMessage(ValidationError, [
       "notNull Violation: CompanyUser.companyUuid cannot be null",
-      "notNull Violation: CompanyUser.userUuid cannot be null"
+      "notNull Violation: CompanyUser.userUuid cannot be null",
+      "notNull Violation: CompanyUser.role cannot be null"
     ]);
   });
 
   it("throws an error if no companyUuid is provided", async () => {
-    const companyUser = new CompanyUser({ userUuid: UUID.generate() });
+    const companyUser = new CompanyUser(omit(mandatoryAttributes, "companyUuid"));
     await expect(companyUser.validate()).rejects.toThrowErrorWithMessage(
       ValidationError,
       "notNull Violation: CompanyUser.companyUuid cannot be null"
@@ -41,10 +44,18 @@ describe("CompanyUser", () => {
   });
 
   it("throws an error if no userUuid is provided", async () => {
-    const companyUser = new CompanyUser({ companyUuid: UUID.generate() });
+    const companyUser = new CompanyUser(omit(mandatoryAttributes, "userUuid"));
     await expect(companyUser.validate()).rejects.toThrowErrorWithMessage(
       ValidationError,
       "notNull Violation: CompanyUser.userUuid cannot be null"
+    );
+  });
+
+  it("throws an error if no role is provided", async () => {
+    const companyUser = new CompanyUser(omit(mandatoryAttributes, "role"));
+    await expect(companyUser.validate()).rejects.toThrowErrorWithMessage(
+      ValidationError,
+      "notNull Violation: CompanyUser.role cannot be null"
     );
   });
 });
