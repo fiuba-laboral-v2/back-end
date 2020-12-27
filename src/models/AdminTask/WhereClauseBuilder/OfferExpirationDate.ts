53e47c2b3c274f106c32e3de726a512ae6849168
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-27 03:30:58 -0300

    Add OfferExpirationDateWhereClause

---
+++ src/models/AdminTask/WhereClauseBuilder/OfferExpirationDate.ts
@@ -0,0 +1,25 @@
+import { Secretary } from "$models/Admin";
+import { AdminTaskType } from "$models/AdminTask/Model";
+import { Offer } from "$models";
+
+export const OfferExpirationDateWhereClause = {
+  build: ({ secretary, modelName }: IStatusWhereClauseProps) => {
+    if (modelName !== AdminTaskType.Offer) return;
+
+    let expirationDateProperty: string;
+    if (secretary === Secretary.graduados) {
+      expirationDateProperty = "graduatesExpirationDateTime";
+    } else {
+      expirationDateProperty = "studentsExpirationDateTime";
+    }
+
+    return `
+      ("${Offer.tableName}"."${expirationDateProperty}" < '${new Date().toISOString()}')
+    `;
+  }
+};
+
+interface IStatusWhereClauseProps {
+  secretary: Secretary;
+  modelName: AdminTaskType;
+}
