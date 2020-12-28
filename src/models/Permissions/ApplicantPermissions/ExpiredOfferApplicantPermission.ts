d97a79e873db512647a9df64a1cc9249eaf5f7cb
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-28 04:33:42 -0300

    Use HasAppliedToOfferApplicantPermission in the ExpiredOfferApplicantPermission

--- src/models/Permissions/ApplicantPermissions/ExpiredOfferApplicantPermission.ts
+++ src/models/Permissions/ApplicantPermissions/ExpiredOfferApplicantPermission.ts
@@ -1,26 +1,26 @@
 import { ApplicantType } from "$models/Applicant";
-import { JobApplicationRepository } from "$models/JobApplication";
 import { Applicant, Offer } from "$models";
 import { OfferTargetApplicantPermission } from "./OfferTargetApplicantPermission";
+import { HasAppliedToOfferApplicantPermission } from "./HasAppliedToOfferApplicantPermission";
 import { IApplicantPermission } from "./Interfaces";
 
 export class ExpiredOfferApplicantPermission implements IApplicantPermission {
-  private readonly applicant: Applicant;
   private readonly offer: Offer;
   private readonly applicantType: ApplicantType;
-  private readonly offerTargetApplicantPermission: OfferTargetApplicantPermission;
+  private readonly offerTargetPermission: OfferTargetApplicantPermission;
+  private readonly hasAppliedToOfferPermission: HasAppliedToOfferApplicantPermission;
 
   constructor(applicant: Applicant, offer: Offer, applicantType: ApplicantType) {
-    this.applicant = applicant;
     this.offer = offer;
     this.applicantType = applicantType;
-    this.offerTargetApplicantPermission = new OfferTargetApplicantPermission(offer, applicantType);
+    this.offerTargetPermission = new OfferTargetApplicantPermission(offer, applicantType);
+    this.hasAppliedToOfferPermission = new HasAppliedToOfferApplicantPermission(applicant, offer);
   }
 
   public async apply() {
-    if (!this.offerTargetApplicantPermission.apply()) return false;
+    if (!this.offerTargetPermission.apply()) return false;
     const isExpired = this.offer.isExpiredFor(this.applicantType);
-    if (isExpired) return JobApplicationRepository.hasApplied(this.applicant, this.offer);
+    if (isExpired) return this.hasAppliedToOfferPermission.apply();
     return true;
   }
 }
