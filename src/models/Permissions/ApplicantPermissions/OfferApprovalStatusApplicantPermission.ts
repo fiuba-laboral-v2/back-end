9c4d4fa6bbc317ea28df688e6b12189305efa271
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-28 03:04:05 -0300

    Add OfferApprovalStatusApplicantPermission

---
+++ src/models/Permissions/ApplicantPermissions/OfferApprovalStatusApplicantPermission.ts
@@ -0,0 +1,27 @@
+import { ApplicantType } from "$models/Applicant";
+import { Offer } from "$models";
+import { some } from "lodash";
+import { ApprovalStatus } from "$models/ApprovalStatus";
+
+export class OfferApprovalStatusApplicantPermission {
+  private readonly offer: Offer;
+  private readonly applicantType: ApplicantType;
+
+  constructor(offer: Offer, applicantType: ApplicantType) {
+    this.offer = offer;
+    this.applicantType = applicantType;
+  }
+
+  public apply() {
+    const approvalStatusProperties = {
+      [ApplicantType.student]: ["extensionApprovalStatus"],
+      [ApplicantType.graduate]: ["graduadosApprovalStatus"],
+      [ApplicantType.both]: ["graduadosApprovalStatus", "extensionApprovalStatus"]
+    }[this.applicantType];
+
+    return some(
+      approvalStatusProperties,
+      columnName => this.offer[columnName] === ApprovalStatus.approved
+    );
+  }
+}
---
+++ test/models/Permissions/ApplicantPermissions/OfferApprovalStatusApplicantPermission.test.ts
@@ -0,0 +1,124 @@
+import { OfferApprovalStatusApplicantPermission } from "$models/Permissions/ApplicantPermissions/OfferApprovalStatusApplicantPermission";
+import { ApplicantType } from "$models/Applicant";
+import { Admin, Offer } from "$models";
+import { UUID } from "$models/UUID";
+import { OfferGenerator } from "$generators/Offer";
+import { ApprovalStatus } from "$models/ApprovalStatus";
+import { set } from "lodash";
+import { Secretary } from "$models/Admin";
+
+describe("OfferApprovalStatusApplicantPermission", () => {
+  const createOfferWith = (target: ApplicantType, status: ApprovalStatus) => {
+    const isStudent = target === ApplicantType.student || target === ApplicantType.both;
+    const isGraduate = target === ApplicantType.graduate || target === ApplicantType.both;
+    const companyUuid = UUID.generate();
+    const attributes = OfferGenerator.data.withObligatoryData({ companyUuid });
+    attributes.targetApplicantType = target;
+    if (isStudent) set(attributes, "extensionApprovalStatus", status);
+    if (isGraduate) set(attributes, "graduadosApprovalStatus", status);
+    return new Offer(attributes);
+  };
+
+  describe("Student", () => {
+    const applicantType = ApplicantType.student;
+
+    it("returns true if the offer is approved for students", () => {
+      const offer = createOfferWith(applicantType, ApprovalStatus.approved);
+      const permissions = new OfferApprovalStatusApplicantPermission(offer, applicantType);
+      expect(permissions.apply()).toBe(true);
+    });
+
+    it("returns false if the offer is rejected for students", () => {
+      const offer = createOfferWith(applicantType, ApprovalStatus.rejected);
+      const permissions = new OfferApprovalStatusApplicantPermission(offer, applicantType);
+      expect(permissions.apply()).toBe(false);
+    });
+
+    it("returns false if the offer is pending for students", () => {
+      const offer = createOfferWith(applicantType, ApprovalStatus.pending);
+      const permissions = new OfferApprovalStatusApplicantPermission(offer, applicantType);
+      expect(permissions.apply()).toBe(false);
+    });
+  });
+
+  describe("Graduate", () => {
+    const applicantType = ApplicantType.graduate;
+
+    it("returns true if the offer is approved for graduates", () => {
+      const offer = createOfferWith(applicantType, ApprovalStatus.approved);
+      const permissions = new OfferApprovalStatusApplicantPermission(offer, applicantType);
+      expect(permissions.apply()).toBe(true);
+    });
+
+    it("returns false if the offer is rejected for graduates", () => {
+      const offer = createOfferWith(applicantType, ApprovalStatus.rejected);
+      const permissions = new OfferApprovalStatusApplicantPermission(offer, applicantType);
+      expect(permissions.apply()).toBe(false);
+    });
+
+    it("returns false if the offer is pending for graduates", () => {
+      const offer = createOfferWith(applicantType, ApprovalStatus.pending);
+      const permissions = new OfferApprovalStatusApplicantPermission(offer, applicantType);
+      expect(permissions.apply()).toBe(false);
+    });
+  });
+
+  describe("Graduate ans students", () => {
+    const applicantType = ApplicantType.both;
+    let extensionAdmin: Admin;
+    let graduadosAdmin: Admin;
+
+    beforeAll(() => {
+      extensionAdmin = new Admin({ userUuid: UUID.generate(), secretary: Secretary.extension });
+      graduadosAdmin = new Admin({ userUuid: UUID.generate(), secretary: Secretary.graduados });
+    });
+
+    it("returns true if the offer is approved for graduates and rejected for students", () => {
+      const offer = createOfferWith(applicantType, ApprovalStatus.approved);
+      offer.updateStatus(graduadosAdmin, ApprovalStatus.approved, 15);
+      offer.updateStatus(extensionAdmin, ApprovalStatus.rejected, 15);
+      const permissions = new OfferApprovalStatusApplicantPermission(offer, applicantType);
+      expect(permissions.apply()).toBe(true);
+    });
+
+    it("returns true if the offer is approved for graduates and pending for students", () => {
+      const offer = createOfferWith(applicantType, ApprovalStatus.approved);
+      offer.updateStatus(graduadosAdmin, ApprovalStatus.approved, 15);
+      offer.updateStatus(extensionAdmin, ApprovalStatus.pending, 15);
+      const permissions = new OfferApprovalStatusApplicantPermission(offer, applicantType);
+      expect(permissions.apply()).toBe(true);
+    });
+
+    it("returns true if the offer is rejected for graduates and approved for students", () => {
+      const offer = createOfferWith(applicantType, ApprovalStatus.approved);
+      offer.updateStatus(graduadosAdmin, ApprovalStatus.rejected, 15);
+      offer.updateStatus(extensionAdmin, ApprovalStatus.approved, 15);
+      const permissions = new OfferApprovalStatusApplicantPermission(offer, applicantType);
+      expect(permissions.apply()).toBe(true);
+    });
+
+    it("returns true if the offer is pending for graduates and approved for students", () => {
+      const offer = createOfferWith(applicantType, ApprovalStatus.approved);
+      offer.updateStatus(graduadosAdmin, ApprovalStatus.pending, 15);
+      offer.updateStatus(extensionAdmin, ApprovalStatus.approved, 15);
+      const permissions = new OfferApprovalStatusApplicantPermission(offer, applicantType);
+      expect(permissions.apply()).toBe(true);
+    });
+
+    it("returns false if the offer is pending for both", () => {
+      const offer = createOfferWith(applicantType, ApprovalStatus.approved);
+      offer.updateStatus(graduadosAdmin, ApprovalStatus.pending, 15);
+      offer.updateStatus(extensionAdmin, ApprovalStatus.pending, 15);
+      const permissions = new OfferApprovalStatusApplicantPermission(offer, applicantType);
+      expect(permissions.apply()).toBe(false);
+    });
+
+    it("returns false if the offer is rejected for both", () => {
+      const offer = createOfferWith(applicantType, ApprovalStatus.approved);
+      offer.updateStatus(graduadosAdmin, ApprovalStatus.rejected, 15);
+      offer.updateStatus(extensionAdmin, ApprovalStatus.rejected, 15);
+      const permissions = new OfferApprovalStatusApplicantPermission(offer, applicantType);
+      expect(permissions.apply()).toBe(false);
+    });
+  });
+});
