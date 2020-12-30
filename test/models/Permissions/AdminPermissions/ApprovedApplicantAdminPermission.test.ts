39e9db3bf3f4784dc2c46483651b0e1e79d48938
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-30 14:07:51 -0300

    Fix test description

--- test/models/Permissions/AdminPermissions/ApprovedApplicantAdminPermission.test.ts
+++ test/models/Permissions/AdminPermissions/ApprovedApplicantAdminPermission.test.ts
@@ -10,19 +10,19 @@ describe("ApprovedApplicantAdminPermission", () => {
     applicant = new Applicant({ padron: 1, userUuid: UUID.generate() });
   });
 
-  it("returns true the applicant is approved", async () => {
+  it("returns true if the applicant is approved", async () => {
     applicant.set({ approvalStatus: ApprovalStatus.approved });
     const permission = new ApprovedApplicantAdminPermission(applicant);
     expect(await permission.apply()).toBe(true);
   });
 
-  it("returns false the applicant is rejected", async () => {
+  it("returns false if the applicant is rejected", async () => {
     applicant.set({ approvalStatus: ApprovalStatus.rejected });
     const permission = new ApprovedApplicantAdminPermission(applicant);
     expect(await permission.apply()).toBe(false);
   });
 
-  it("returns false the applicant is pending", async () => {
+  it("returns false if the applicant is pending", async () => {
     applicant.set({ approvalStatus: ApprovalStatus.pending });
     const permission = new ApprovedApplicantAdminPermission(applicant);
     expect(await permission.apply()).toBe(false);
