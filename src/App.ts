7572b9eb64ac986ce266a2725d3407125141ea76
Dylan Alvarez <dylanalvarez1995@gmail.com>
2021-01-05 03:29:55 -0300

    Fix image upload size error

--- src/App.ts
+++ src/App.ts
@@ -5,12 +5,14 @@ import { Logger } from "./libs/Logger";
 import { Environment } from "./config/Environment";
 import { ApolloServer } from "./server";
 import { JWT } from "./JWT";
+import { json } from "express";
 
 Logger.info(`Running on ${Environment.NODE_ENV()} environment`);
 
 const App: express.Express = express();
 App.use(cors());
 App.use(cookieParser());
+App.use(json({ limit: "1mb" }));
 ApolloServer.applyMiddleware({ app: App, path: "/graphql" });
 JWT.applyMiddleware({ app: App });
 
