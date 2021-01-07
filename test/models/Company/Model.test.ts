0f3e547136c9e0bf4eddea86a16ee93d45a67d92
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-07 16:35:12 -0300

    Fix indentation

--- test/models/Company/Model.test.ts
+++ test/models/Company/Model.test.ts
@@ -10,13 +10,18 @@ import {
   NameWithDigitsError,
   WrongLengthCuitError
 } from "validations-fiuba-laboral-v2";
+import { CuitGenerator } from "$generators/Cuit";
 
 describe("Company", () => {
+  const mandatoryAttributes = {
+    cuit: CuitGenerator.generate(),
+    companyName: "companyName",
+    businessName: "businessName"
+  };
+
   it("creates a valid company", async () => {
     const companyData = {
-      cuit: "30711819017",
-      companyName: "companyName",
-      businessName: "businessName",
+      ...mandatoryAttributes,
       slogan: "slogan",
       description: "description",
       logo: "https://logo.png",
@@ -26,16 +31,14 @@ describe("Company", () => {
     };
     const company = new Company(companyData);
     await expect(company.validate()).resolves.not.toThrow();
-    expect(company).toEqual(
-      expect.objectContaining({
-        uuid: expect.stringMatching(UUID_REGEX),
-        ...companyData
-      })
-    );
+    expect(company).toBeObjectContaining({
+      uuid: expect.stringMatching(UUID_REGEX),
+      ...companyData
+    });
   });
 
   it("throws an error if cuit is null", async () => {
-    const company = new Company({ cuit: null, companyName: "devartis", businessName: "el zorro" });
+    const company = new Company({ ...mandatoryAttributes, cuit: null });
     await expect(company.validate()).rejects.toThrowErrorWithMessage(
       ValidationError,
       "notNull Violation: Company.cuit cannot be null"
@@ -43,11 +46,7 @@ describe("Company", () => {
   });
 
   it("throws an error if companyName is null", async () => {
-    const company: Company = new Company({
-      cuit: "30711819017",
-      companyName: null,
-      businessName: "el zorro"
-    });
+    const company: Company = new Company({ ...mandatoryAttributes, companyName: null });
     await expect(company.validate()).rejects.toThrowErrorWithMessage(
       ValidationError,
       "notNull Violation: Company.companyName cannot be null"
@@ -55,11 +54,7 @@ describe("Company", () => {
   });
 
   it("throws an error if businessName is null", async () => {
-    const company: Company = new Company({
-      cuit: "30711819017",
-      companyName: "devartis",
-      businessName: null
-    });
+    const company: Company = new Company({ ...mandatoryAttributes, businessName: null });
     await expect(company.validate()).rejects.toThrowErrorWithMessage(
       ValidationError,
       "notNull Violation: Company.businessName cannot be null"
@@ -77,39 +72,27 @@ describe("Company", () => {
   });
 
   it("throws an error if cuit has invalid format", async () => {
-    const company = new Company({
-      cuit: "30711819018",
-      companyName: "devartis",
-      businessName: "el zorro2"
-    });
+    const company = new Company({ ...mandatoryAttributes, cuit: "30711819018" });
     await expect(company.validate()).rejects.toThrow(InvalidCuitError.buildMessage());
   });
 
   it("throws an error if cuit has less than eleven digits", async () => {
-    const company = new Company({ cuit: "30", companyName: "devartis", businessName: "el zorro" });
+    const company = new Company({ ...mandatoryAttributes, cuit: "30" });
     await expect(company.validate()).rejects.toThrow(WrongLengthCuitError.buildMessage());
   });
 
   it("throws an error if cuit has more than eleven digits", async () => {
-    const company = new Company({
-      cuit: "3057341761199",
-      companyName: "devartis",
-      businessName: "el zorro"
-    });
+    const company = new Company({ ...mandatoryAttributes, cuit: "3057341761199" });
     await expect(company.validate()).rejects.toThrow(WrongLengthCuitError.buildMessage());
   });
 
   it("throws an error if companyName is empty", async () => {
-    const company = new Company({ cuit: "30711819017", companyName: "", businessName: "el zorro" });
+    const company = new Company({ ...mandatoryAttributes, companyName: "" });
     await expect(company.validate()).rejects.toThrow(EmptyNameError.buildMessage());
   });
 
-  it("should throw an error if companyName has digits", async () => {
-    const company = new Company({
-      cuit: "30711819017",
-      companyName: "Google34",
-      businessName: "el zorro"
-    });
+  it("throws an error if companyName has digits", async () => {
+    const company = new Company({ ...mandatoryAttributes, companyName: "Google34" });
     await expect(company.validate()).rejects.toThrow(NameWithDigitsError.buildMessage());
   });
 
@@ -126,7 +109,7 @@ describe("Company", () => {
   });
 
   it("throws an error if businessName is empty", async () => {
-    const company = new Company({ cuit: "30711819017", companyName: "devartis", businessName: "" });
+    const company = new Company({ ...mandatoryAttributes, businessName: "" });
     await expect(company.validate()).rejects.toThrowErrorWithMessage(
       ValidationError,
       "Validation error: Validation notEmpty on businessName failed"
@@ -134,12 +117,7 @@ describe("Company", () => {
   });
 
   it("throws an error if url has invalid format", async () => {
-    const company = new Company({
-      cuit: "30711819017",
-      companyName: "Google34",
-      businessName: "el zorro",
-      website: "badURL"
-    });
+    const company = new Company({ ...mandatoryAttributes, website: "badURL" });
     await expect(company.validate()).rejects.toThrowErrorWithMessage(
       ValidationError,
       InvalidURLError.buildMessage()
@@ -148,12 +126,7 @@ describe("Company", () => {
 
   it("throws an error if email has invalid format", async () => {
     const badEmail = "badEmail";
-    const company = new Company({
-      cuit: "30711819017",
-      companyName: "Google34",
-      businessName: "el zorro",
-      email: badEmail
-    });
+    const company = new Company({ ...mandatoryAttributes, email: badEmail });
     await expect(company.validate()).rejects.toThrowErrorWithMessage(
       ValidationError,
       InvalidEmailError.buildMessage(badEmail)
