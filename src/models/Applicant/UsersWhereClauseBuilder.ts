bd918e7f4f3464656a5775395872715d66789506
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-08 13:32:28 -0300

    Use NameWhereClause in the UsersWhereClauseBuilder

--- src/models/Applicant/UsersWhereClauseBuilder.ts
+++ src/models/Applicant/UsersWhereClauseBuilder.ts
@@ -1,33 +1,13 @@
 import { UserSequelizeModel } from "$models";
-import { col, fn, Op, where } from "sequelize";
+import { NameWhereClause } from "$models/QueryBuilder";
 import { Includeable } from "sequelize/types/lib/model";
 
 export const UsersWhereClauseBuilder = {
   build: ({ name }: IBuild): Includeable | undefined => {
     if (name === undefined) return;
-    const removeAccent = word => word.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-    const normalize = word => removeAccent(word).toLowerCase();
-    const words = name
-      .replace(/\n/g, " ")
-      .split(" ")
-      .filter(word => word !== "");
-    if (words.length === 0) return;
-    return {
-      model: UserSequelizeModel,
-      where: {
-        [Op.and]: words.map(word => ({
-          [Op.or]: [
-            where(fn("unaccent", fn("lower", col("name"))), {
-              [Op.regexp]: `(^|[[:space:]])${normalize(word)}([[:space:]]|$)`
-            }),
-            where(fn("unaccent", fn("lower", col("surname"))), {
-              [Op.regexp]: `(^|[[:space:]])${normalize(word)}([[:space:]]|$)`
-            })
-          ]
-        }))
-      },
-      attributes: []
-    };
+    const whereClause = NameWhereClause.build({ name, columnNames: ["name", "surname"] });
+    if (!whereClause) return;
+    return { model: UserSequelizeModel, where: whereClause, attributes: [] };
   }
 };
 
