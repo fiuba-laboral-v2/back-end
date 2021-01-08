8e42a055410f4bc1f842f679cc635522a8c46edb
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-08 16:49:31 -0300

    Add CompanyWhereClauseBuilder

--- src/models/QueryBuilder/index.ts
+++ src/models/QueryBuilder/index.ts
@@ -2,3 +2,4 @@ export * from "./NameWhereClause";
 export * from "./UsersWhereClauseBuilder";
 export * from "./ApplicantWhereClauseBuilder";
 export * from "./ApplicantCareersWhereClauseBuilder";
+export * from "./CompanyWhereClauseBuilder";
---
+++ test/models/QueryBuilder/CompanyWhereClauseBuilder.test.ts
@@ -0,0 +1,91 @@
+import { CompanyWhereClauseBuilder } from "$models/QueryBuilder";
+import { Company } from "$models";
+import { col, fn, Op, where } from "sequelize";
+
+describe("CompanyWhereClauseBuilder", () => {
+  it("returns undefined if no name is provided", () => {
+    const clause = CompanyWhereClauseBuilder.build({});
+    expect(clause).toBeUndefined();
+  });
+
+  it("returns undefined if the name is a newline character", () => {
+    const clause = CompanyWhereClauseBuilder.build({ companyName: "" });
+    expect(clause).toBeUndefined();
+  });
+
+  it("returns undefined if the name is en empty array", () => {
+    const clause = CompanyWhereClauseBuilder.build({ companyName: "" });
+    expect(clause).toBeUndefined();
+  });
+
+  it("returns undefined if the name is only spaces", () => {
+    const clause = CompanyWhereClauseBuilder.build({ companyName: "     " });
+    expect(clause).toBeUndefined();
+  });
+
+  it("returns undefined if the name is multiple new lines", () => {
+    const clause = CompanyWhereClauseBuilder.build({ companyName: "\n\n\n\n\n\n\n\n\n\n" });
+    expect(clause).toBeUndefined();
+  });
+
+  it("returns a clause for a name with capitalize letters", () => {
+    const companyName = "Buddy Guy";
+    const clause = CompanyWhereClauseBuilder.build({ companyName });
+    expect(clause).toEqual({
+      model: Company,
+      where: {
+        [Op.and]: [
+          {
+            [Op.or]: [
+              where(fn("unaccent", fn("lower", col("companyName"))), {
+                [Op.regexp]: `(^|[[:space:]])buddy([[:space:]]|$)`
+              })
+            ]
+          },
+          {
+            [Op.or]: [
+              where(fn("unaccent", fn("lower", col("companyName"))), {
+                [Op.regexp]: `(^|[[:space:]])guy([[:space:]]|$)`
+              })
+            ]
+          }
+        ]
+      },
+      attributes: []
+    });
+  });
+
+  it("returns a clause for a name with accents", () => {
+    const companyName = "námé with áccent";
+    const clause = CompanyWhereClauseBuilder.build({ companyName });
+    expect(clause).toEqual({
+      model: Company,
+      where: {
+        [Op.and]: [
+          {
+            [Op.or]: [
+              where(fn("unaccent", fn("lower", col("companyName"))), {
+                [Op.regexp]: `(^|[[:space:]])name([[:space:]]|$)`
+              })
+            ]
+          },
+          {
+            [Op.or]: [
+              where(fn("unaccent", fn("lower", col("companyName"))), {
+                [Op.regexp]: `(^|[[:space:]])with([[:space:]]|$)`
+              })
+            ]
+          },
+          {
+            [Op.or]: [
+              where(fn("unaccent", fn("lower", col("companyName"))), {
+                [Op.regexp]: `(^|[[:space:]])accent([[:space:]]|$)`
+              })
+            ]
+          }
+        ]
+      },
+      attributes: []
+    });
+  });
+});
