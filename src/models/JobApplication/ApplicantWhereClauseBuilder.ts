0e39879c77980cf0ef5b4c1311d858fedcad42a9
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-08 16:14:21 -0300

    Add ApplicantWhereClauseBuilder

---
+++ src/models/JobApplication/ApplicantWhereClauseBuilder.ts
@@ -0,0 +1,26 @@
+import { UserSequelizeModel, Applicant } from "$models";
+import { NameWhereClause } from "$models/QueryBuilder";
+import { Includeable } from "sequelize/types/lib/model";
+
+export const ApplicantWhereClauseBuilder = {
+  build: ({ applicantName }: IBuild): Includeable | undefined => {
+    if (applicantName === undefined) return;
+    const where = NameWhereClause.build({ name: applicantName, columnNames: ["name", "surname"] });
+    if (!where) return;
+    return {
+      model: Applicant,
+      include: [
+        {
+          model: UserSequelizeModel,
+          where,
+          attributes: []
+        }
+      ],
+      attributes: []
+    };
+  }
+};
+
+interface IBuild {
+  applicantName?: string;
+}
