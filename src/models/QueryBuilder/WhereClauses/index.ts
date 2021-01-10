31a0e1e6475dbb47ad13cde87598769df4c3b30c
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-10 00:16:28 -0300

    Move WhereClauses to their own directory

---
+++ src/models/QueryBuilder/WhereClauses/index.ts
@@ -0,0 +1,6 @@
+export * from "./NameWhereClause";
+export * from "./UsersWhereClauseBuilder";
+export * from "./ApplicantWhereClauseBuilder";
+export * from "./ApplicantCareersWhereClauseBuilder";
+export * from "./CompanyWhereClauseBuilder";
+export * from "./OfferWhereClauseBuilder";
--- src/models/QueryBuilder/index.ts
+++ src/models/QueryBuilder/index.ts
@@ -1,6 +1,1 @@
-export * from "./NameWhereClause";
-export * from "./UsersWhereClauseBuilder";
-export * from "./ApplicantWhereClauseBuilder";
-export * from "./ApplicantCareersWhereClauseBuilder";
-export * from "./CompanyWhereClauseBuilder";
-export * from "./OfferWhereClauseBuilder";
+export * from "./WhereClauses";
