f431b9832cac2e15fdaa3da7c197b2451894c0fd
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-12 20:11:28 -0300

    Add production seeder for SharedSettings

---
+++ src/seeders/20210112200842-production-add-shared-settings.ts
@@ -0,0 +1,25 @@
+import { QueryInterface } from "sequelize";
+import { Environment } from "../config/Environment";
+
+const TABLE_NAME = "SharedSettings";
+
+export = {
+  up: async (queryInterface: QueryInterface) => {
+    if (Environment.NODE_ENV() !== Environment.PRODUCTION) return;
+    return queryInterface.bulkInsert(TABLE_NAME, [
+      {
+        uuid: "6b228e77-9e8e-4438-872e-f3714a5846dd",
+        companySignUpAcceptanceCriteria:
+          "Esta empresa estará pendiente de aprobación por personal de FIUBA",
+        companyEditableAcceptanceCriteria:
+          "Esta empresa estará pendiente de aprobación por personal de FIUBA",
+        editOfferAcceptanceCriteria:
+          "Esta oferta estará pendiente de aprobación por personal de FIUBA"
+      }
+    ]);
+  },
+  down: async (queryInterface: QueryInterface) => {
+    if (Environment.NODE_ENV() !== Environment.PRODUCTION) return;
+    return queryInterface.bulkDelete(TABLE_NAME, {});
+  }
+};
