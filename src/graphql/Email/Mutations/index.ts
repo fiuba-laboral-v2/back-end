19c0d65cb681dc18c711453ee0520c3436046e18
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-12 19:06:12 -0300

    Remove sendEmail mutation

--- src/graphql/Email/Mutations/index.ts
+++
@@ -1,5 +0,0 @@
-import { sendEmail } from "./sendEmail";
-
-export const emailMutations = {
-  sendEmail
-};
--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -2,7 +2,6 @@ import { merge } from "lodash";
 import { companyMutations } from "./Company";
 import { offerMutations } from "./Offer";
 import { applicantMutations } from "./Applicant";
-import { emailMutations } from "./Email";
 import { jobApplicationMutations } from "./JobApplication";
 import { careerMutations } from "./Career";
 import { userMutations } from "./User";
@@ -17,7 +16,6 @@ export const mutations = () =>
     companyMutations,
     userMutations,
     offerMutations,
-    emailMutations,
     jobApplicationMutations,
     adminSettingsMutations,
     adminMutations,
