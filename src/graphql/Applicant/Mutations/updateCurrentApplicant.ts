8bdebafafa49b5cb2ce80f205984cc8d71c14680
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-31 19:51:25 -0300

    Move transactional operation to the updateCurrentApplicant resolver

--- src/graphql/Applicant/Mutations/updateCurrentApplicant.ts
+++ src/graphql/Applicant/Mutations/updateCurrentApplicant.ts
@@ -7,6 +7,14 @@ import { GraphQLUserUpdateInput } from "$graphql/User/Types/GraphQLUserUpdateInp
 import { ApplicantRepository, IApplicantEditable } from "$models/Applicant";
 import { GraphQLLinkInput } from "../Types/Link";
 import { IApolloServerContext } from "$graphql/Context";
+import { Database } from "$config";
+import { ApprovalStatus } from "$models/ApprovalStatus";
+import { UserRepository } from "$models/User";
+import { ApplicantKnowledgeSectionRepository } from "$models/Applicant/ApplicantKnowledgeSection";
+import { ApplicantExperienceSectionRepository } from "$models/Applicant/ApplicantExperienceSection";
+import { ApplicantLinkRepository } from "$models/Applicant/Link";
+import { ApplicantCareersRepository } from "$models/Applicant/ApplicantCareer";
+import { ApplicantCapabilityRepository } from "$models/ApplicantCapability";
 
 export const updateCurrentApplicant = {
   type: GraphQLApplicant,
@@ -33,6 +41,43 @@ export const updateCurrentApplicant = {
       type: List(GraphQLLinkInput)
     }
   },
-  resolve: async (_: undefined, props: IApplicantEditable, { currentUser }: IApolloServerContext) =>
-    ApplicantRepository.update({ ...props, uuid: currentUser.getApplicantRole().applicantUuid })
+  resolve: async (
+    _: undefined,
+    {
+      user: userAttributes = {},
+      description,
+      knowledgeSections = [],
+      experienceSections = [],
+      links = [],
+      capabilities: newCapabilities = [],
+      careers = []
+    }: IApplicantEditable,
+    { currentUser }: IApolloServerContext
+  ) => {
+    const { applicantUuid } = currentUser.getApplicantRole();
+    const applicant = await ApplicantRepository.findByUuid(applicantUuid);
+    await applicant.set({ description });
+    if (applicant.isRejected()) await applicant.set({ approvalStatus: ApprovalStatus.pending });
+    const user = await UserRepository.findByUuid(applicant.userUuid);
+    user.setAttributes(userAttributes);
+
+    return Database.transaction(async transaction => {
+      await UserRepository.save(user, transaction);
+      await new ApplicantKnowledgeSectionRepository().update({
+        sections: knowledgeSections,
+        applicant,
+        transaction
+      });
+      await new ApplicantExperienceSectionRepository().update({
+        sections: experienceSections,
+        applicant,
+        transaction
+      });
+      await ApplicantLinkRepository.update(links, applicant, transaction);
+      await ApplicantCareersRepository.update(careers, applicant, transaction);
+      await ApplicantCapabilityRepository.update(newCapabilities, applicant, transaction);
+      await applicant.save({ transaction });
+      return applicant;
+    });
+  }
 };
