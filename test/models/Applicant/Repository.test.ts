7d8c47e14fe61284fdd80b81e5cd188ad66db328
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-30 21:08:24 -0300

    fix tests

--- test/models/Applicant/Repository.test.ts
+++ test/models/Applicant/Repository.test.ts
@@ -795,7 +795,7 @@ describe("ApplicantRepository", () => {
     });
 
     it("updates email", async () => {
-      await expectToUpdateProperty("user.email", "newEmail@gmail.com");
+      await expectToUpdateProperty("user.email", "aNewEmail@gmail.com");
     });
 
     it("updates description", async () => {
